f9bc50a1eb298a013a87dfa83a66afa7a6d77efd
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-02 23:48:22 +0100

    Update 5-print_string.py

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-string = "Holberton School"
-multiplied_string = string * 3
-print(multiplied_string)
-substring = string[:9]
-print(substring)
+str = "Holberton School"
+print(3 * str)
+print(str[:9])
