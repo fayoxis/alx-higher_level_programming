9f749f07b6c6481e0f5694a090655463fef3b3cd
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-02 23:50:00 +0100

    Update 6-concat.py

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,3 +1,6 @@
-str1 = "Welcome to"
-str2 = " Holberton School!"
-print(str1, str2, sep='')
+#!/usr/bin/python3
+
+str1 = "Holberton"
+str2 = "School"
+str1 = str1 + " " + str2
+print(f"Welcome to {str1}!")
