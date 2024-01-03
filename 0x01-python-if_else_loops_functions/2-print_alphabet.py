0bbcf1a5fe3993735f472f1c6e788d250ac6b547
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-03 01:40:22 +0100

    Update 2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 """Print the alphabet in lowercase, not followed by a new line."""
 
-output = ""
-
 for letter in range(97, 123):
-    output += "{}".format(chr(letter))
-
-print(output, end="")
+    print("{}".format(chr(letter)), end="")
