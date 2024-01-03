ac0fd3386e34303964fd31cabd56a01ffa412626
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-03 02:42:32 +0100

    Update 13-insert_number.c

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -5,29 +5,34 @@
  * @head: A pointer to the head of the linked list.
  * @number: The number to insert.
  * Return: If the function fails - NULL.
- * Otherwise - a pointer to the new node.
+ *         Otherwise - a pointer to the new node.
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new;
+    listint_t *current = *head, *new_node;
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
+    new_node = malloc(sizeof(listint_t));
+    if (new_node == NULL)
+        return NULL;
+    new_node->n = number;
 
-	if (node == NULL || node->n >= number)
-	{
-		new->next = node;
-		*head = new;
-		return (new);
-	}
+    /* If the list is empty or the number is smaller than the head,*/
+    if (current == NULL || current->n >= number)
+    {
+        new_node->next = current;
+        *head = new_node;
+        return new_node;
+    }
 
-	while (node && node->next && node->next->n < number)
-		node = node->next;
+    /* Find the appropriate position to insert the new node */
+    while (current->next && current->next->n < number)
+    {
+        current = current->next;
+    }
 
-	new->next = node->next;
-	node->next = new;
+    /* Insert the new node */
+    new_node->next = current->next;
+    current->next = new_node;
 
-	return (new);
+    return new_node;
 }
