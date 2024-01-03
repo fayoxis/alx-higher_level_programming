6898f62d9f7969a434ae476941cf8e8cd7018d87
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-03 02:28:45 +0100

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,8 +3,7 @@
 
 def fizzbuzz():
     output = []
-    number = 1
-    while number <= 100:
+    for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
             output.append("FizzBuzz")
         elif number % 3 == 0:
@@ -13,5 +12,4 @@ def fizzbuzz():
             output.append("Buzz")
         else:
             output.append(str(number))
-        number += 1
     return ' '.join(output)
