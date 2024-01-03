3a193910e6a59c2b7fae20cc73124809f42a2687
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-03 02:01:29 +0100

    Update 3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 # Author - Godswill Kalu
 for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) != 'e':
+    if chr(letter) not in ['q', 'e']:
         print("{}".format(chr(letter)), end="")
