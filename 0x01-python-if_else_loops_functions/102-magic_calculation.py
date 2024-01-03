e230f72ff4970e163ea138845e461d3c979a940a
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-03 02:51:25 +0100

    Update 102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
 
 def magic_calculation(a, b, c):
-    """Match bytecode provided by Holberton School."""
     if a < b:
-        return (c)
-    if c > b:
-        return (a + b)
-    return (a*b - c)
+        result = c
+    elif c > b:
+        result = a + b
+    else:
+        result = a * b - c
+
+    return result
