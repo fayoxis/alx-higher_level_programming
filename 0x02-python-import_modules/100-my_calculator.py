f3d57c5f6f03cbdd1789fda5a4bf14e293dff4fb
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-04 21:25:40 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,15 +3,15 @@ import sys
 from calculator_1 import add, sub, mul, div
 
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
-        sys.exit(1)
+if len(sys.argv) != 4:
+    print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
+    sys.exit(1)
 
     a = int(sys.argv[1])
     operator = sys.argv[2]
     b = int(sys.argv[3])
 
-    if operator == '+':
+if operator == '+':
         result = add(a, b)
     elif operator == '-':
         result = sub(a, b)
@@ -19,8 +19,8 @@ def main():
         result = mul(a, b)
     elif operator == '/':
         result = div(a, b)
-    else:
-        print("Unknown operator. Available operators: +, -, *, and /")
+else:
+    print("Unknown operator. Available operators: +, -, *, and /")
         sys.exit(1)
 
     print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
