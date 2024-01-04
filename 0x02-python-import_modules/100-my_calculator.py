d388c435f0e5e039f2d58d4616236de88ce7d3f2
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-04 21:23:30 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,19 +2,30 @@
 from sys import argv
 from calculator_1 import add, sub, mul, div
 
-if __name__ != "__main__":
-    exit()
-
-argc = len(argv) - 1
-if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-else:
-    operator = argv[2]
-    if operator in ['+', '-', '*', '/']:
-        func = {'+': add, '-': sub, '*': mul, '/': div}[operator]
-        result = func(int(argv[1]), int(argv[3]))
-        print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), operator, int(argv[3]), result))
+def calculate_result(a, operator, b):
+    if operator == '+':
+        return add(a, b)
+    elif operator == '-':
+        return sub(a, b)
+    elif operator == '*':
+        return mul(a, b)
+    elif operator == '/':
+        return div(a, b)
     else:
+        return None
+
+if __name__ == "__main__":
+    if len(argv) != 4:
+        print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
+        exit(1)
+    
+    a = int(argv[1])
+    operator = argv[2]
+    b = int(argv[3])
+    
+    result = calculate_result(a, operator, b)
+    if result is None:
         print("Unknown operator. Available operators: +, -, *, and /")
         exit(1)
+    
+    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
