7e64427ce047a0a9244e44075e17109cfbf80217
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-04 21:18:09 +0100

    Update 5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 from variable_load_5 import a
 
-if __name__ != "__main__":
+if __name__ == "__main__":
+    print("{:d}".format(a))
+else:
     exit()
-
-print("{:d}".format(a))
