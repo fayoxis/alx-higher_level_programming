e13243768fd5283e685c5517328b012123d2e90f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-04 21:29:14 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,29 @@
 #!/usr/bin/python3
-from sys import argv
+import sys
 from calculator_1 import add, sub, mul, div
-if __name__ != "__main__":
-    exit()
 
-argc = len(argv) - 1
-if argc != 3:
-    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
-    exit(1)
-elif argv[2] == '+':
-    func = add
-elif argv[2] == '-':
-    func = sub
-elif argv[2] == '*':
-    func = mul
-elif argv[2] == '/':
-    func = div
-else:
+def main():
+    if len(sys.argv) != 4:
+    print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
+        sys.exit(1)
+
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+    else:
     print("Unknown operator. Available operators: +, -, *, and /")
-    exit(1)
+        sys.exit(1)
 
-result = func(int(argv[1]), int(argv[3]))
-print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
-    argv[2], int(argv[3]), result))
+    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
+
+if __name__ == "__main__":
+    main()
