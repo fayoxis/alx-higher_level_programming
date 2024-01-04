3d9e87d2b3df3772574e30c229f84bab2150a625
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-04 21:21:38 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,17 +9,12 @@ argc = len(argv) - 1
 if argc != 3:
     print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
     exit(1)
-elif argv[2] == '+':
-    func = add
-elif argv[2] == '-':
-    func = sub
-elif argv[2] == '*':
-    func = mul
-elif argv[2] == '/':
-    func = div
 else:
-    print("Unknown operator. Available operators: +, -, *, and /")
-    exit(1)
-
-result = func(int(argv[1]), int(argv[3]))
-print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), argv[2], int(argv[3]), result))
+    operator = argv[2]
+    if operator in ['+', '-', '*', '/']:
+        func = {'+': add, '-': sub, '*': mul, '/': div}[operator]
+        result = func(int(argv[1]), int(argv[3]))
+        print("{:d} {:s} {:d} = {:d}".format(int(argv[1]), operator, int(argv[3]), result))
+    else:
+        print("Unknown operator. Available operators: +, -, *, and /")
+        exit(1)
