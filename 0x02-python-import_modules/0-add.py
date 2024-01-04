0f70f228c5c7d8bf4f7542d2772592fc35f724f1
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-04 20:57:17 +0100

    Update 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
+from add_0 import add
 
 if __name__ == "__main__":
-    from add_0 import add
-
     y = 1
     u = 2
-    print("{} + {} = {}".format(y, u, add(y, u)))
-
+    print("{:d} + {:d} = {:d}".format(y, u, add(y, u)))
