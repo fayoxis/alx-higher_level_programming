0be34eaea2c05649dc8726fb4544bc955311f74f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-04 21:08:42 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 import sys
 import hidden_4 as hidden
+
 def main():
-if __name__ != "__main__":
-exit()
-for name in dir(hidden):
-if name[0:2] != "__":
-print(name)
+    if __name__ != "__main__":
+    exit()
+
+    for name in dir(hidden):
+        if name[0:2] != "__":
+        print(name)
+
 if __name__ == "__main__":
-main()
+    main()
