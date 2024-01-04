4f359f2c0269e833d345bbcc94170fb93ac52903
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-04 21:27:34 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,29 +1,28 @@
 #!/usr/bin/python3
-import sys
+from sys import argv
 from calculator_1 import add, sub, mul, div
 
-def main():
-if len(sys.argv) != 4:
-    print("Usage: {:s} <a> <operator> <b>".format(sys.argv[0]))
-    sys.exit(1)
+if __name__ != "__main__":
+    exit()
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+argc = len(argv) - 1
 
-if operator == '+':
-        result = add(a, b)
-    elif operator == '-':
-        result = sub(a, b)
-    elif operator == '*':
-        result = mul(a, b)
-    elif operator == '/':
-        result = div(a, b)
+if argc != 3:
+    output = "Usage: {:s} <a> <operator> <b>".format(argv[0])
+    exit(1)
+elif argv[2] == '+':
+    func = add
+elif argv[2] == '-':
+    func = sub
+elif argv[2] == '*':
+    func = mul
+elif argv[2] == '/':
+    func = div
 else:
-    print("Unknown operator. Available operators: +, -, *, and /")
-        sys.exit(1)
+    output = "Unknown operator. Available operators: +, -, *, and /"
+    exit(1)
 
-    print("{:d} {:s} {:d} = {:d}".format(a, operator, b, result))
-
-if __name__ == "__main__":
-    main()
+result = func(int(argv[1]), int(argv[3]))
+output = "{:d} {:s} {:d} = {:d}".format(int(argv[1]),
+argv[2], int(argv[3]), result)
+print(output)
