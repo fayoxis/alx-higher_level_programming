bf6b06a99e320b3684fc5d59950f41acca8159ff
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-04 21:28:13 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
 from calculator_1 import add, sub, mul, div
-
 if __name__ != "__main__":
     exit()
 
 argc = len(argv) - 1
-
 if argc != 3:
-    output = "Usage: {:s} <a> <operator> <b>".format(argv[0])
+    print("Usage: {:s} <a> <operator> <b>".format(argv[0]))
     exit(1)
 elif argv[2] == '+':
     func = add
@@ -19,10 +17,9 @@ elif argv[2] == '*':
 elif argv[2] == '/':
     func = div
 else:
-    output = "Unknown operator. Available operators: +, -, *, and /"
+    print("Unknown operator. Available operators: +, -, *, and /")
     exit(1)
 
 result = func(int(argv[1]), int(argv[3]))
-output = "{:d} {:s} {:d} = {:d}".format(int(argv[1]),
-argv[2], int(argv[3]), result)
-print(output)
+print("{:d} {:s} {:d} = {:d}".format(int(argv[1]),
+    argv[2], int(argv[3]), result))
