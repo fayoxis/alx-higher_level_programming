539d429c60f1face0f52ead8280a7c1bf5dfdef9
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-08 17:35:30 +0100

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,8 +3,8 @@
 def replace_in_list(my_list, idx, element):
     list_len = len(my_list)
     if not (0 <= idx < list_len):
-        return my_list.copy()
+        return my_list[:]
 
-    new_list = my_list.copy()
+    new_list = my_list[:]
     new_list[idx] = element
     return new_list
