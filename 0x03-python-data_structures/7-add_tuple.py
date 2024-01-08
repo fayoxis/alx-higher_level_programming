c02f7efa67f717f74b90329d5f2391fa15c0d6d7
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-08 17:51:37 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,14 +5,14 @@ def add_tuple(tuple_a=(), tuple_b=()):
     ta_len = len(tuple_a)
     tb_len = len(tuple_b)
     new_tup = ()
-    
+
     for i in range(2):
         val_a = tuple_a[i] if i < ta_len else 0
         val_b = tuple_b[i] if i < tb_len else 0
-        
+
         if i == 0:
             new_tup = val_a + val_b
         else:
             new_tup = (new_tup, val_a + val_b)
-    
+
     return new_tup
