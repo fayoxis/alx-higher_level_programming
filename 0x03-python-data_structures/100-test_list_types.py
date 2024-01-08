6cf11788895cfb1a139a7c18d44488a30ce44c76
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-08 19:13:08 +0100

    Update 100-test_list_types.py

--- 0x03-python-data_structures/100-test_list_types.py
+++ 0x03-python-data_structures/100-test_list_types.py
@@ -6,7 +6,7 @@ l = ['hello', 'World']
 lib.print_python_list_info(l)
 del l[1]
 lib.print_python_list_info(l)
-l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "Holberton"]
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "my project only"]
 lib.print_python_list_info(l)
 l = []
 lib.print_python_list_info(l)
