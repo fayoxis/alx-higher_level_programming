6acd7d765a271632f30ee85b432ed4f4fed5cf3a
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-08 17:51:58 +0100

    Update 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,13 +2,6 @@
 
 
 def multiple_returns(sentence):
-    """
-    Returns a tuple with the length of a string and its first
-    character
-    """
     s_len = len(sentence)
-    if s_len == 0:
-        f_char = None
-    else:
-        f_char = sentence[0]
-    return ((s_len, f_char))
+    f_char = sentence[0] if s_len > 0 else None
+    return (s_len, f_char)
