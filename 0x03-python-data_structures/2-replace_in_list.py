63a0634696aef56287028b0907d112adcc23f4d3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-08 17:36:21 +0100

    Update 2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    list_len = len(my_list)
-    if not (0 <= idx < list_len):
-        return my_list[:]
+list_len = len(my_list)
+if list_len <= idx or idx < 0:
+    return (my_list)
 
-    new_list = my_list[:]
-    new_list[idx] = element
-    return new_list
+my_list[idx] = element
+return (my_list)
