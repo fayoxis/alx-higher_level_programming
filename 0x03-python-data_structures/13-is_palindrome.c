2838245c21940525cf42320d8f5e40bedf41ccac
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-08 18:38:54 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,99 +1,86 @@
 #include "lists.h"
 #include <stdio.h>
 
-void reverseLinkedList(Node **head);
-int areListsEquivalent(listint_t *list1, listint_t *list2);
+void reverse_list(listint_t **head);
+int listequiv(listint_t *l1, listint_t *l2);
 
 /**
- * isPalindrome - checks if a linked list is a palindrome
+ * ispalindrome - checks if a linked list is a palindrome
  * @head: double pointer to the head of the linked list
  *
- * Return: 0 (not a palindrome), 1 (is a palindrome)
+ * Return: 0 (not palindrome) 1 (is palindrome)
  */
-int isPalindrome(listint_t **head)
+int ispalindrome(listint_t **head)
 {
-listint_t *slow, *fast, *prev_slow, *first_half, *second_half, *mid;
+	listint_t *skip_1, *skip_2, *prev_s1, *first_half, *second_half, *mid;
 
-if (!head || !(*head) || !((*head)->next))
-    return 1;
+	if (!head || !(*head) || !((*head)->next))
+		return (1);
 
-slow = fast = prev_slow = *head;
-second_half = mid = NULL;
+	first_half = skip_1 = skip_2 = prev_s1 = *head;
+	second_half = mid = NULL;
 
-do
-{
-    fast = fast->next;
-    if (fast)
-    {
-        fast = fast->next;
-        prev_slow = slow;
-        slow = slow->next;
-    }
-} while (fast && fast->next);
+	while (skip_1 && skip_2 && skip_2->next)
+	{
+		prev_s1 = skip_1;
+		skip_1 = skip_1->next;
+		skip_2 = skip_2->next->next;
+	}
+	if (skip_2 == NULL) /* Even # of nodes */
+		second_half = skip_1;
+	else
+	{
+		mid = skip_1;
+		second_half = skip_1->next;
+	}
+	prev_s1->next = NULL;
+	reverse_list(&second_half);
 
-if (fast == NULL)
-    second_half = slow;
-else
-{
-    mid = slow;
-    second_half = slow->next;
-}
-
-prev_slow->next = NULL;
-reverseList(&second_half);
-
-if (areListsEqual(*head, second_half))
-    return 1;
-else
-    return 0;
+	if (list_equiv(first_half, second_half))
+		return (1);
+	else
+		return (0);
 }
 
 /**
- * areListsEquivalent - checks if two linked lists contain identical data and are
- *                     the same length as each other
+ * list_equiv - checks if two linked lists contain identical data and are
+ * the same length as each other
  * @list1: list one to compare to list two
  * @list2: list two to compare to list one
  *
  * Return: 1 (equivalent) 0 (not equal)
  */
-int areListsEquivalent(listint_t *list1, listint_t *list2)
+int listequiv(listint_t *list1, listint_t *list2)
 {
-    while (list1 != NULL && list2 != NULL)
-    {
-        if (list1->data != list2->data)
-            return 0;
-        
-        list1 = list1->next;
-        list2 = list2->next;
-    }
-    
-    if (list1 != NULL || list2 != NULL)
-        return 0;
-    
-    return 1;
+	while (list1 || list2)
+	{
+		if (list1->n != list2->n || !list1 || !list2)
+			return (0);
+		if (list1)
+			list1 = list1->next;
+		if (list2)
+			list2 = list2->next;
+	}
+	return (1);
 }
-/**
- * reverseLinkedList - reverses a linked list in place
- * @head: double pointer to the head of the linked list
- *
- * This function reverses the order of a linked list by modifying the pointers
- * of each node. The head pointer itself is modified to point to the new head
- * of the reversed list.
- *
- * Return: void
- */
-void reverseLinkedList(Node **head)
-{
-    Node *current = *head;
-    Node *previous = NULL;
-    Node *nextNode = NULL;
 
-    while (current != NULL)
-    {
-        nextNode = current->next;
-        current->next = previous;
-        previous = current;
-        current = nextNode;
-    }
-    *head = previous;
+/**
+ * reverse_list - reverses a linked list
+ * @head: double pointer to head of linked list so we can modify it
+ *
+ * Return: always void, modifies head itself.
+ */
+void reverselist(listint_t **head)
+{
+	listint_t *next = NULL, *prev = NULL, *cur;
+
+	cur = *head;
+	while (cur)
+	{
+		next = cur->next;
+		cur->next = prev;
+		prev = cur;
+		cur = next;
+	}
+	*head = prev;
 }
