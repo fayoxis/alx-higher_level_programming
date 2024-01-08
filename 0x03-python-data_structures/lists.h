710ecf29cc0d28e3f20dc97a2b2f1068cb708811
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-08 18:20:06 +0100

    Update lists.h

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -19,6 +19,6 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 
-int is_palindrome(listint_t **head);
+int isPalindrome(listint_t **head);
 
 #endif /* LISTS_H */
