26e0f32431e32ee0929c12aad81f000589a9d76f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-08 18:08:16 +0100

    Update 11-delete_at.py

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,12 +2,9 @@
 
 
 def delete_at(my_list=[], idx=0):
-    """
-    deletes an element from a list at a given index
-    """
-    l_len = len(my_list)
-    if idx >= l_len or idx < 0:
-        return (my_list)
+    list_length = len(my_list)
+    if idx >= list_length or idx < 0:
+        return my_list
 
     del my_list[idx]
-    return (my_list)
+    return my_list
