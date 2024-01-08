13f2f9d4fb3c846d48ba415acc02759d78c0dfdd
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-08 18:04:03 +0100

    Update 9-max_integer.py

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+
 def max_integer(my_list=[]):
+
     if len(my_list) == 0:
         return None
-    return max(my_list)
+    max_value = float('-inf')
+    for num in my_list:
+        if num > max_value:
+            max_value = num
+    return max_value
