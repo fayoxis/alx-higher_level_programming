0907660b14509c13bc694bb1ea3367007be2fadd
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-08 18:23:21 +0100

    Update 13-is_palindrome.c

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,87 +1,95 @@
 #include "lists.h"
 #include <stdio.h>
 
-void reverse_list(listint_t **head);
-int list_equiv(listint_t *l1, listint_t *l2);
+void reverseLinkedList(Node **head);
+int areListsEquivalent(listint_t *list1, listint_t *list2);
 
 /**
- * is_palindrome - checks if a linked list is a palindrome
+ * isPalindrome - checks if a linked list is a palindrome
  * @head: double pointer to the head of the linked list
  *
- * Return: 0 (not palindrome) 1 (is palindrome)
+ * Return: 0 (not a palindrome), 1 (is a palindrome)
  */
-int is_palindrome(listint_t **head)
+int isPalindrome(listint_t **head)
 {
-	listint_t *skip_1, *skip_2, *prev_s1, *first_half, *second_half, *mid;
+    listint_t *slow, *fast, *prev_slow, *first_half, *second_half, *mid;
 
-	/* linked lists of length 0 and 1 are palindromes by default */
-	if (!head || !(*head) || !((*head)->next))
-		return (1);
+    if (!head || !(*head) || !((*head)->next))
+        return 1;
 
-	first_half = skip_1 = skip_2 = prev_s1 = *head;
-	second_half = mid = NULL;
+    slow = fast = prev_slow = *head;
+    second_half = mid = NULL;
 
-	while (skip_1 && skip_2 && skip_2->next)
-	{
-		prev_s1 = skip_1;
-		skip_1 = skip_1->next;
-		skip_2 = skip_2->next->next;
-	}
-	if (skip_2 == NULL) /* Even # of nodes */
-		second_half = skip_1;
-	else /* odd number of nodes, there is a middle node */
-	{
-		mid = skip_1;
-		second_half = skip_1->next;
-	}
-	prev_s1->next = NULL; /* null terminate first half */
-	reverse_list(&second_half);
+    while (fast && fast->next)
+    {
+        fast = fast->next->next;
+        prev_slow = slow;
+        slow = slow->next;
+    }
 
-	if (list_equiv(first_half, second_half))
-		return (1); /* equivalent lists, palindrome found */
-	else
-		return (0);
+    if (fast == NULL)
+        second_half = slow;
+    else
+    {
+        mid = slow;
+        second_half = slow->next;
+    }
+
+    prev_slow->next = NULL;
+    reverseList(&second_half);
+
+    if (areListsEqual(*head, second_half))
+        return 1;
+    else
+        return 0;
 }
 
 /**
- * list_equiv - checks if two linked lists contain identical data and are
- * the same length as each other
- * @l1: list one to compare to list two
- * @l2: list two to compare to list one
+ * areListsEquivalent - checks if two linked lists contain identical data and are
+ *                     the same length as each other
+ * @list1: list one to compare to list two
+ * @list2: list two to compare to list one
  *
  * Return: 1 (equivalent) 0 (not equal)
  */
-int list_equiv(listint_t *l1, listint_t *l2)
+int areListsEquivalent(listint_t *list1, listint_t *list2)
 {
-	while (l1 || l2)
-	{
-		if (l1->n != l2->n || !l1 || !l2)
-			return (0);
-		if (l1)
-			l1 = l1->next;
-		if (l2)
-			l2 = l2->next;
-	}
-	return (1);
+    while (list1 != NULL && list2 != NULL)
+    {
+        if (list1->data != list2->data)
+            return 0;
+        
+        list1 = list1->next;
+        list2 = list2->next;
+    }
+    
+    if (list1 != NULL || list2 != NULL)
+        return 0;
+    
+    return 1;
 }
-
 /**
- * reverse_list - reverses a linked list
- * @head: double pointer to head of linked list so we can modify it
+ * reverseLinkedList - reverses a linked list in place
+ * @head: double pointer to the head of the linked list
  *
- * Return: always void, modifies head itself.
+ * This function reverses the order of a linked list by modifying the pointers
+ * of each node. The head pointer itself is modified to point to the new head
+ * of the reversed list.
+ *
+ * Return: void
  */
-void reverse_list(listint_t **head)
+void reverseLinkedList(Node **head)
 {
-	listint_t *next = NULL, *prev = NULL, *cur;
+    Node *current = *head;
+    Node *previous = NULL;
+    Node *nextNode = NULL;
 
-	cur = *head;
-	while (cur)
-	{
-		next = cur->next;
-		cur->next = prev;
-		prev = cur;
-		cur = next;
-	}
-	*head = prev;
+    while (current != NULL)
+    {
+        nextNode = current->next;
+        current->next = previous;
+        previous = current;
+        current = nextNode;
+    }
+    *head = previous;
 }
