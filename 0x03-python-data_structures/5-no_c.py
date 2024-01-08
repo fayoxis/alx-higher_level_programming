15ea040df08a6ac1777d93c09331cb0bfdc1adb6
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-08 17:47:29 +0100

    Update 5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 
-
 def no_c(my_string):
-    """
-    removes all instances of 'c' & 'C' from string
-    """
     new_str = ""
-    for i in range(len(my_string)):
-        if my_string[i] != 'c' and my_string[i] != 'C':
-            new_str += my_string[i]
-    return (new_str)
+    for char in my_string:
+        if char.lower() != 'c':
+            new_str += char
+    return new_str
