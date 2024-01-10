7bbdac73992effa8d2af79b0feed9af20a414a26
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 12:00:36 +0100

    Update 8-simple_delete.py

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -2,9 +2,6 @@
 
 
 def simple_delete(a_dictionary, key=""):
-    """
-    deletes an element based on the key from a dictionary
-    """
-    if key is not "" and key in a_dictionary:
+    if key != "" and key in a_dictionary:
         a_dictionary.pop(key)
-    return (a_dictionary.copy())
+    return a_dictionary.copy()
