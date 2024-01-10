3b9ab1be914835d3b1651d9d89eb726bad15f9d8
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 14:26:22 +0100

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -3,9 +3,9 @@
 def weight_average(my_list=[]):
     if len(my_list) == 0:
         return 0
-    
+
     score_weight_pairs = [(score * weight, weight) for score, weight in my_list]
     total_score = sum(score for score, _ in score_weight_pairs)
     total_weight = sum(weight for _, weight in score_weight_pairs)
-    
+
     return total_score / total_weight
