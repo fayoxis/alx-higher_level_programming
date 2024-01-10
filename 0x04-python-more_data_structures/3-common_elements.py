ca90ffaf2120425ebdad81c38ca1e88508f42071
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 11:27:48 +0100

    Update 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-
-def common_elements(set_1, set_2):
-    """
-    returns a set of common elements from two sets
-    """
-    return ({ele for ele in set_1 if ele in set_2})
+def uniq_add(my_list=[]):
+    unique_elements = {ele for ele in my_list}
+    return sum(unique_elements)
