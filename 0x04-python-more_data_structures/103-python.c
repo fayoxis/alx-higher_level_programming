a037000a43323699ac21ca3b9db03cbbc55bbf21
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 20:25:17 +0100

    Update 103-python.c

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -2,10 +2,10 @@
 #include <Python.h>
 
 /**
- * print_python_bytes - it Prints bytes information
+ * print_python_bytes - Prints information about a bytes object
  *
- * @p: it is Python Object
- * Return: it return nothing : return
+ * @p: Python bytes object
+ * Return: void
  */
 void print_python_bytes(PyObject *p)
 {
@@ -13,6 +13,8 @@ void print_python_bytes(PyObject *p)
     long int size, i, limit;
 
     printf("[.] bytes object info\n");
+    
+    /* Check if the object is a valid bytes object */
     if (!PyBytes_Check(p))
     {
         printf("  [ERROR] Invalid Bytes Object\n");
@@ -25,6 +27,7 @@ void print_python_bytes(PyObject *p)
     printf("  size: %ld\n", size);
     printf("  trying string: %s\n", string);
 
+    /* Determine the limit for printing the bytes */
     if (size >= 10)
         limit = 10;
     else
@@ -32,24 +35,23 @@ void print_python_bytes(PyObject *p)
 
     printf("  first %ld bytes:", limit);
 
-    i = 0;
-    while (i < limit)
+    /* Print each byte in hexadecimal format */
+    for (i = 0; i < limit; i++)
     {
         if (string[i] >= 0)
             printf(" %02x", string[i]);
         else
             printf(" %02x", 256 + string[i]);
-        i++;
     }
 
     printf("\n");
 }
 
 /**
- * print_python_list - it Prints the list information
+ * print_python_list - Prints information about a Python list object
  *
- * @p: it is the Python Object
- * Return: it returns nothing :return
+ * @p: Python list object
+ * Return: void
  */
 void print_python_list(PyObject *p)
 {
@@ -64,13 +66,12 @@ void print_python_list(PyObject *p)
     printf("[*] Size of the Python List = %ld\n", size);
     printf("[*] Allocated = %ld\n", list->allocated);
 
-    i = 0;
-    while (i < size)
+    /* Iterate over the list elements and print their information */
+    for (i = 0; i < size; i++)
     {
         obj = list->ob_item[i];
         printf("Element %ld: %s\n", i, Py_TYPE(obj)->tp_name);
         if (PyBytes_Check(obj))
             print_python_bytes(obj);
-        i++;
     }
 }
