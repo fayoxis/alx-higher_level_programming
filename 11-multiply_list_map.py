52eed519c1f235edc08e907d3abf6ca47babb942
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 12:19:22 +0100

    Update 11-multiply_list_map.py

--- 11-multiply_list_map.py
+++ 11-multiply_list_map.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-def mutiply_list_map(my_list=[], number=0):
-    return (list(map(lambda x: x*number, my_list)))
+
+def multiply_list_map(my_list=[], number=0):
+    multiplied_list = []
+    for n in my_list:
+        multiplied_list.append(n * number)
+    return multiplied_list
