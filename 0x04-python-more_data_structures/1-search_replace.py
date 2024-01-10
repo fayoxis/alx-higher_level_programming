7eee16ddbc055781b7a44ffe75dddbeecbcf8053
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 11:10:24 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-
 def search_replace(my_list, search, replace):
-    """
-    replaces all occurances of search with replace
-    """
-
-    return ([ele if ele != search else replace for ele in my_list])
+    result = []
+    for ele in my_list:
+        if ele != search:
+            result.append(ele)
+        else:
+            result.append(replace)
+    return result
