acb0b4809a0b0b5253b96c90d7c26819725de1f3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 11:53:48 +0100

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 
-
 def update_dictionary(a_dictionary, key, value):
-    """
-    updates a dictionary and returns a new copy
-    original is affected
-    """
-    a_dictionary.update({key: value})
-    return (a_dictionary.copy())
+    new_dictionary = a_dictionary.copy()
+    new_dictionary[key] = value
+    return new_dictionary
