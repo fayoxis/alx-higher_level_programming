0c16bdd8203fc0171fa93ae1df2d3c4700df33f5
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 20:41:38 +0100

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 
-def complex_delete(a_dictionary, value):
-    if a_dictionary and isinstance(a_dictionary, dict):
-        to_del = {key: val for key, val in a_dictionary.items() if value == val}
-        for key in to_del:
-            del a_dictionary[key]
-        return a_dictionary.copy()
+def complex_delete(my_dict, value):
+    for k, v in list(my_dict.items()):
+        if value == v:
+            my_dict.pop(k)
+    return my_dict
