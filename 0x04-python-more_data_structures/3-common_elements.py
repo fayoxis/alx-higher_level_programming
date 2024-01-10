95f0be2eea2a79c98596af61e5445f79a9698118
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 11:33:39 +0100

    Update 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-
 def common_elements(set_1, set_2):
-    return ({ele for ele in set_1 if ele in set_2})
+    common = {ele for ele in set_1 if ele in set_2}
+    return common
