c7876b9c49c3715d1b5b7636afecc6edce96a888
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 11:36:04 +0100

    Update 4-only_diff_elements.py

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-def find_symmetric_difference(set_1, set_2):
-    symmetric_difference = set_1 ^ set_2
-    return {element for element in symmetric_difference if (element in set_1 and element not in set_2) or
-            (element in set_2 and element not in set_1)}
+
+def only_diff_elements(set_1, set_2):
+    unique_elements = set_1.symmetric_difference(set_2)
+    return unique_elements
