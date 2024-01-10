3b30337eb27e10a0200425970646764b541ee79b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 11:32:46 +0100

    Update 3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 
 
-def uniq_add(my_list=None):
-    if my_list is None:
-        my_list = []
-    return sum({ele for ele in my_list})
+def common_elements(set_1, set_2):
+    return ({ele for ele in set_1 if ele in set_2})
