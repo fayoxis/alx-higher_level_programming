e7d0950daac716ec4a342270f5cdbf5dfaeeb255
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 20:43:44 +0100

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 
-def complex_delete(my_dict, value):
-    for k, v in list(my_dict.items()):
+def complex_delete(mya_dictionary, value):
+    for k, v in list(mya_dictionary.items()):
         if value == v:
-            my_dict.pop(k)
-    return my_dict
+            mya_dictionary.pop(k)
+    return mya_dictionary
