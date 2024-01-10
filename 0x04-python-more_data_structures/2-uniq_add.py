604b1d7d1731b635576fcd5615346f582cf7ea37
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 11:14:27 +0100

    Update 2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-
 def uniq_add(my_list=[]):
-    """
-    adds all unique elements of a list of ints together
-    """
-    return (sum({ele for ele in my_list}))
+    unique_elements = {ele for ele in my_list}
+    return sum(unique_elements)
