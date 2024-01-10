04ebe17cf6cae2726521c6587309921aa0feaf4e
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 12:49:07 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,37 +1,31 @@
 #!/usr/bin/python3
 
 def convert_roman(ch):
-    """
-    Converts a Roman numeral character into the respective integer.
-    """
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    return roman_dict.get(ch, -1)
+    conversions = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    return conversions.get(ch, -1)
 
 
-def roman_to_int(roman_string):
-    """
-    Converts a string of Roman numerals to decimal.
-    """
-    if roman_string is None or not isinstance(roman_string, str):
-        return 0
-    
+    def roman_to_int(roman_string):
     cur_max = -1
     cur = conv = 0
     holder = []
-    
+
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
+
     for c in roman_string:
         cur = convert_roman(c)
-        
         if cur == -1:
             return 0
-        
+
         if len(holder) == 0:
             if cur == cur_max or cur_max == -1:
                 cur_max = cur
                 conv += cur
             elif cur < cur_max:
                 holder.append(cur)
-            elif cur > cur_max:
+            elif cur > cur_max:  # Only happens if smaller is starting number
+                # For example: IIX, VXC
                 cur_max = cur
                 cur -= conv
                 conv = cur
@@ -43,8 +37,8 @@ def roman_to_int(roman_string):
                 holder.clear()
             else:
                 holder.append(cur)
-    
+
     if len(holder) != 0:
         conv += sum(holder)
-    
+
     return conv
