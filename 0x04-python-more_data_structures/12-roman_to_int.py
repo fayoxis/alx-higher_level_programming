ed8a37761ee1059800b7c8c14666cd69aafbd702
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 12:44:27 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,22 @@
-#!/usr/bin/python3
-
 def convert_roman(ch):
-    conversion_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    return conversion_dict.get(ch, -1)
+    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    return roman_dict.get(ch, -1)
+
 
 def roman_to_int(roman_string):
+    if roman_string is None or not isinstance(roman_string, str):
+        return 0
+    
     cur_max = -1
     cur = conv = 0
     holder = []
-
-    if roman_string is None or type(roman_string) is not str:
-        return 0
-
+    
     for c in roman_string:
         cur = convert_roman(c)
+        
         if cur == -1:
             return 0
-
+        
         if len(holder) == 0:
             if cur == cur_max or cur_max == -1:
                 cur_max = cur
@@ -35,8 +35,8 @@ def roman_to_int(roman_string):
                 holder.clear()
             else:
                 holder.append(cur)
-
+    
     if len(holder) != 0:
         conv += sum(holder)
-
+    
     return conv
