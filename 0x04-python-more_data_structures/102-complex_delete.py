fd4880de32c43553ba84923bfc331a022b2367f1
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 14:32:55 +0100

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -2,9 +2,8 @@
 
 
 def complex_delete(a_dictionary, value):
-    if a_dictionary is not None and isinstance(a_dictionary, dict):
-        items = list(a_dictionary.items())
-        to_del = {key: val for (key, val) in items if value == val}
-        for (key, val) in to_del.items():
+    if isinstance(a_dictionary, dict):
+        to_delete = {key: val for key, val in a_dictionary.items() if val == value}
+        for key in to_delete:
             del a_dictionary[key]
         return a_dictionary.copy()
