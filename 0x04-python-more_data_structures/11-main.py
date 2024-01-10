fd01c4c5db88b4330e927bef71ac4e6d55b75ef6
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 12:35:11 +0100

    Update 11-main.py

--- 0x04-python-more_data_structures/11-main.py
+++ 0x04-python-more_data_structures/11-main.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-multiply_list_map = __import__('11-multiply_list_map').multiply_list_map
+from 11_multiply_list_map import multiply_list_map
 
 my_list = [1, 2, 3, 4, 6]
 new_list = multiply_list_map(my_list, 4)
