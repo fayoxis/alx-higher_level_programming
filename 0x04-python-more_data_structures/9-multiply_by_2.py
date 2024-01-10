b21a72eff34afbe61ac704d1bbf6b5800e82b020
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-10 12:01:24 +0100

    Update 9-multiply_by_2.py

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-
 def multiply_by_2(a_dictionary):
-    """
-    using dictionary comprehension for my first time
-    in order to multiply the value of every key: value
-    pair in a dictionary
-    """
-    return {key: value*2 for (key, value) in a_dictionary.items()}
+    multiplied_dictionary = {}
+    for key, value in a_dictionary.items():
+        multiplied_dictionary[key] = value * 2
+    return multiplied_dictionary
