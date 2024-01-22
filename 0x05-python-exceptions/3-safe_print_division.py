e46df10fc78246b08f15a304d5eee99d2f99c9db
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-22 23:10:51 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-from __future__ import print_function
-
 
 def safe_print_division(a, b):
     try:
         res = a / b
-    except:
+    except ZeroDivisionError:
         res = None
     finally:
-        print("Inside result: {}".format(res))
+        print(f"Inside result: {res}")
     return res
