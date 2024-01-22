c674c3ff3d548559d98b61afaf7833403653ab7c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-22 22:41:23 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     element_count = 0
     try:
-        while my_list[element_count] and element_count < x:
+        while element_count < x and my_list[element_count]:
             print(my_list[element_count], end="")
             element_count += 1
     except (IndexError, TypeError):
