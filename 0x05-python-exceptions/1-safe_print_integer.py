173cd69879cd565449f3e339c5f2bddd4f9fd5f1
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-22 22:44:12 +0100

    Update 1-safe_print_integer.py

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-
-
 def safe_print_integer(value):
     try:
-        print("{:d}".format(value))
+        output = "{:d}".format(value)
+        print(output)
         return True
-    except:
+    except (TypeError, ValueError):
         return False
