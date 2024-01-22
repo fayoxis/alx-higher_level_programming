c5ccba2f32116364a8d5d470a345aea40d12fb0d
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-22 23:08:22 +0100

    Update 3-safe_print_division.py

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 
-
 def safe_print_division(a, b):
-    """
-    divides two integers and prints the result
-    catches divide by zero exception
-    """
     try:
         res = a / b
     except:
