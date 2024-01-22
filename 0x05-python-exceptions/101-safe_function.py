ce80231f7683c301e6f8ab67aa4c461fef92cc8e
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-22 23:28:08 +0100

    Create 101-safe_function.py

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def safe_function(fct, *args):
+    try:
+        result = fct(*args)
+        return result
+    except Exception as e:
+        sys.stderr.write("Exception: {}\n".format(e))
+        return None
