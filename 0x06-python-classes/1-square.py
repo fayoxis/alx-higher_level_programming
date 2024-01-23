de87e549d206d90d678da63b8d3149403579471b
fayoxis <dtchayearthur@gmail.com>
2024-01-23 11:47:56 +0100

    new python

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """
+    class square that has attributes:
+        size
+    """
+    def __init__(self, size):
+        """
+        the initialization function for the square class
+        """
+        self.__size = size
