f2faa1486a107e94e1401738b47f035ab2b7d587
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-23 13:01:46 +0100

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python3
+"""Class Square that defines a square"""
 
 
 class Square:
-    """
-    class square that has attributes:
-        size
-    some attributes are protected from input.
-    """
+    """Class Square that defines a square"""
+
     def __init__(self, size=0):
+        """Initialize square
+
+        Args:
+            size (int): size of the square
         """
-        initialization function for our square clasee
-        """
-        if type(size) != int:
-            raise TypeError("size must be an integer")
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
         elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         else:
-            self.__size = size
+            self.__size = size  # size of the square
 
     def area(self):
-        """
-        calculates the area of the square
+        """Returns the area.
+
+        Returns:
+            area.
         """
         return self.__size ** 2
+
+    def __repr__(self):
+        """Returns a string representation of the square.
+
+        Returns:
+            str: string representation of the square.
+        """
+        return f'Square(side={self.__size})'
+
+
+# Test the Square class
+if __name__ == "__main__":
+    square = Square(5)
+    print(square.area())
+    print(square)
