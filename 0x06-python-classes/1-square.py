073b5491189e01891f3d30378476a6a023afcc90
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-23 12:48:05 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -19,9 +19,9 @@ class Square:
             TypeError: If the size is not an integer.
             ValueError: If the size is less than 0.
         """
-        if not isinstance(size, int):
+        if type(size) is not int:
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
         else:
-            self.__size = size
+            self.__size = size  # Size of the square
