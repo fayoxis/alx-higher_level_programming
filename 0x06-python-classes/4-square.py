5e87bdb9a36c915b2442d29807b06a3a10deeb53
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-23 13:09:35 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,41 +1,50 @@
 #!/usr/bin/python3
+
+""" This program defines a class Square that represents shape"""
+
 class Square:
-    """A class representing a square"""
+    """
+    This class represents a square shape.
+    """
     def __init__(self, size=0):
-        """Initializes the square instance.
+        """
+        Initializes a square object.
 
         Args:
-        size (int): The size of the square.
+            size (int): The size of the square. Default is 0.
         """
         self.size = size
 
     @property
     def size(self):
-        """Gets the size of the square."""
+        """
+        Gets the size of the square.
+
+        Returns:
+            int: The size of the square.
+        """
         return self.__size
 
     @size.setter
     def size(self, value):
-        """Sets the size of the square.
+        """
+        Sets the size of the square.
 
         Args:
-        value (int): The size value to set.
-
-        Raises:
-        TypeError: If value is not an integer.
-        ValueError: If value is less than 0.
+            value (int): The size of the square.
         """
         if not isinstance(value, int):
-            raise TypeError("size must be an integer")
+            raise TypeError('size must be an integer')
         elif value < 0:
-            raise ValueError("size must be >= 0")
+            raise ValueError('size must be >= 0')
         else:
             self.__size = value
 
     def area(self):
-        """Calculates the area of the square.
+        """
+        Calculates the area of the square.
 
         Returns:
-        int: The area of the square.
+            int: The area of the square.
         """
-        return self.__size * self.__size
+        return self.__size ** 2
