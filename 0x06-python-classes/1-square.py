5379d3609d5221340b8d9472055d1dc286d92e96
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-23 12:48:33 +0100

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,27 +1,18 @@
 #!/usr/bin/python3
-""" 
-Square class definition.
-"""
+""" class Square that defines a square"""
+
 
 class Square:
-    """ 
-    Class that represents a square.
-    """
+    """ class Square that defines a square"""
 
     def __init__(self, size=0):
-        """ 
-        Initializes an instance of the Square class.
-
+        """initialize square
         Args:
-            size (int): The size of the square. Defaults to 0.
-        
-        Raises:
-            TypeError: If the size is not an integer.
-            ValueError: If the size is less than 0.
+            size (int): size of the square
         """
         if type(size) is not int:
             raise TypeError('size must be an integer')
         elif size < 0:
             raise ValueError('size must be >= 0')
         else:
-            self.__size = size  # Size of the square
+            self.__size = size  #: size of the square
