374aec3c2eb1fefbfb14806f1be80c868ac00936
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-23 21:02:45 +0100

    Update 103-magic_class.py

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-"""Define a Circle class with radius and diameter attributes."""
+"""Defines a MagicClass that represents a circle."""
 
 import math
 
 
-class Circle:
-    """Represent a circle."""
+class MagicClass:
+    """Represents a circle."""
 
     def __init__(self, radius=0):
-        """Initialize a Circle instance.
+        """Initializes a MagicClass.
 
         Args:
-            radius (float or int): The radius of the Circle.
+            radius (float or int): The radius of the MagicClass.
         """
         self.__radius = 0
         if not isinstance(radius, (int, float)):
@@ -19,9 +19,9 @@ class Circle:
         self.__radius = radius
 
     def area(self):
-        """Return the area of the Circle."""
+        """Returns the area of the MagicClass."""
         return self.__radius ** 2 * math.pi
 
     def circumference(self):
-        """Return the circumference of the Circle."""
+        """Returns the circumference of the MagicClass."""
         return 2 * math.pi * self.__radius
