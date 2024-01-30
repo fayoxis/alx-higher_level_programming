6c641cad9ef2133c0d20d84cafff4f27c5c01b5f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 11:28:47 +0100

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    if not hasattr(magic_string, 'count'):
-        magic_string.count = 0
-    magic_string.count += 1
-    return ("BestSchool, " * (magic_string.count - 1) + "BestSchool")
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
