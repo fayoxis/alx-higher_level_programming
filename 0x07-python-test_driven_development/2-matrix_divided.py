9fdff9c257eac4e2284e8796630469a04f200015
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 19:08:42 +0100

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-"""
-Defines a function that divides all elements of a matrix.
+"""Defines a function that divides all elements of a matrix.
+
+Attributes:
+    matrix_divided: divides all elements of a matrix.
 """
 
+
 def matrix_divided(matrix, div):
-    """
-    Divides all elements of a matrix.
+    """Divides all elements of a matrix.
 
     Args:
         matrix (list): A list of lists of integers or floats.
-        div (int/float): The value to divide each element of the matrix by.
+        div (int/float): Value to divide by.
 
     Raises:
-        TypeError: If the matrix is not a list of lists of integers or floats.
-        TypeError: If each row of the matrix does not have the same size.
+        TypeError: If matrix is not a list of lists of integers or floats.
+        TypeError: If each row of the matrix isn't of the same size.
         TypeError: If an element of any list is not an integer or float.
         TypeError: If a row in the matrix is not a list.
-        TypeError: If the divisor is not an integer or a float.
-        ZeroDivisionError: If the divisor is equal to 0.
+        TypeError: If div is not an integer or a float.
+        ZeroDivisionError: If div is equal to 0.
 
     Returns:
-        list: A new matrix with the result of dividing each element by the divisor.
+        matrix: A result of the division.
     """
     row_size = None
-    message = "The matrix must be a matrix (list of lists) of integers/floats"
+    message = "matrix must be a matrix (list of lists) of integers/floats"
 
     if not matrix or not isinstance(matrix, list):
         raise TypeError(message)
@@ -42,17 +44,11 @@ def matrix_divided(matrix, div):
             raise TypeError("Each row of the matrix must have the same size")
 
     if not isinstance(div, int) and not isinstance(div, float):
-        raise TypeError("The divisor must be a number")
+        raise TypeError("div must be a number")
 
     if div == 0:
-        raise ZeroDivisionError("Division by zero is not allowed")
+        raise ZeroDivisionError("division by zero")
 
     new_matrix = [[round(j / div, 2) for j in i] for i in matrix]
 
     return new_matrix
-
-# Example usage
-matrix = [[1, 2, 3], [4, 5, 6]]
-divisor = 3
-result = matrix_divided(matrix, divisor)
-print(result)
