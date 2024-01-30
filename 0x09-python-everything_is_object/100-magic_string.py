3a37c24d4e331f572ea60b995a367e6cb8f86deb
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 11:24:57 +0100

    Update 100-magic_string.py

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+
 class MagicString:
     def __init__(self):
         self.n = 0
 
-    def __call__(self):
+    def magic_string(self):
         self.n += 1
         return ("BestSchool, " * (self.n - 1) + "BestSchool")
 
-magic_string = MagicString()
+magic = MagicString()
+magic_string = magic.magic_string
