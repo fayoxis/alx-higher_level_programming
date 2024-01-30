1cfa573f32381372bc6598dd552b6520ac1c5cda
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 20:19:35 +0100

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-"""Defines a function that prints my name.
+"""Defines a function that prints a given name.
 
 Attributes:
-    say_my_name: function that prints my name.
+    say_given_name: function that prints a given name.
 """
 
 
-def say_my_name(first_name, last_name=""):
-    """Prints my name.
+def say_given_name(given_name, family_name=""):
+    """Prints a given name.
 
     Args:
-        first_name (str): My first name.
-        last_name (str, optional): My second name. Defaults to "".
+        given_name (str): The given name.
+        family_name (str, optional): The family name. Defaults to "".
 
     Raises:
-        TypeError: If either first name or last name is not a string.
+        TypeError: If either given name or family name is not a string.
+
+    Prints:
+        The name in the format "The name is {given_name} {family_name}".
     """
-    if not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
+    if not isinstance(given_name, str):
+        raise TypeError("given_name must be a string")
 
-    if not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
+    if not isinstance(family_name, str):
+        raise TypeError("family_name must be a string")
 
-    print("My name is {} {}".format(first_name, last_name))
+    print("The name is {} {}".format(given_name, family_name))
