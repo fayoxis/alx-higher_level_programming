e5516be2c1d0cea85f250252bf40a0ffceb1546c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 21:07:11 +0100

    Update 102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -3,31 +3,36 @@
 #include <Python.h>
 
 /**
- * print_python_string - Function that prints strings in Python
- * @p: Pointer to python object.
- *
- * Return: No return.
+ * print_python_strings - Function that prints information
+ * about Python strings
+ * @objects: Array of PyObject pointers representing Python objects.
+ * @size: Size of the array.
+ * This function iterates over an array of PyObject pointers
+ * and prints information
+ * about each Python string object in the array.
  */
-void print_python_string(PyObject *p)
+void print_python_strings(PyObject **objects, int size)
 {
-	PyObject *str, *repr;
-
-	(void)repr;
-	printf("[.] string object info\n");
-
-	if (strcmp(p->ob_type->tp_name, "str"))
-	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
-	}
-
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
-	else
-		printf("  type: compact unicode object\n");
-
-	repr = PyObject_Repr(p);
-	str = PyUnicode_AsEncodedString(p, "utf-8", "~E~");
-	printf("  length: %ld\n", PyUnicode_GET_SIZE(p));
-	printf("  value: %s\n", PyBytes_AsString(str));
+PyObject *str, *repr;
+(void)repr;
+printf("[.] Python string object information\n");
+for (int i = 0; i < size; i++) {
+/* Check if the object is a string */
+if (strcmp(objects[i]->ob_type->tp_name, "str"))
+{
+printf("  [ERROR] Invalid String Object\n");
+continue;
+}
+/* Check if the string is compact ASCII or compact Unicode */
+if (PyUnicode_IS_COMPACT_ASCII(objects[i]))
+printf("  type: compact ASCII\n");
+else
+printf("  type: compact Unicode object\n");
+/* Get the representation and encoded string value */
+repr = PyObject_Repr(objects[i]);
+str = PyUnicode_AsEncodedString(objects[i], "utf-8", "~E~");
+/* Print the length and value of the string */
+printf("  length: %ld\n", PyUnicode_GET_SIZE(objects[i]));
+printf("  value: %s\n", PyBytes_AsString(str));
+}
 }
