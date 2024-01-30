4d49eb1f7b13e904898b3b647342245ffd521a34
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 21:57:27 +0100

    Update 102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -6,34 +6,20 @@
 /**
  * print_python_string - Function that prints information
  * about Python strings
- * @objects: Array of PyObject pointers representing Python objects.
- * @size: Size of the array.
- * This function iterates over an array of PyObject pointers
- * and prints information
- * about each Python string object in the array.
+ * @p: Array of PyObject pointers representing Python objects.
  */
-void print_python_string(PyObject *objects)
+void print_python_string(PyObject *p)
 {
-PyObject *str, *repr;
-(void)repr;
-printf("[.] Python string object information\n");
-for (int i = 0; i < size; i++) {
-/* Check if the object is a string */
-if (strcmp(objects[i]->ob_type->tp_name, "str"))
-{
-printf("  [ERROR] Invalid String Object\n");
-continue;
-}
-/* Check if the string is compact ASCII or compact Unicode */
-if (PyUnicode_IS_COMPACT_ASCII(objects[i]))
-printf("  type: compact ASCII\n");
-else
-printf("  type: compact Unicode object\n");
-/* Get the representation and encoded string value */
-repr = PyObject_Repr(objects[i]);
-str = PyUnicode_AsEncodedString(objects[i], "utf-8", "~E~");
-/* Print the length and value of the string */
-printf("  length: %ld\n", PyUnicode_GET_SIZE(objects[i]));
-printf("  value: %s\n", PyBytes_AsString(str));
-}
+	wprintf(L"[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str"))
+	{
+		wprintf(L"  [ERROR] Invalid String Object\n");
+		return;
+	}
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		wprintf(L"  type: compact ascii\n");
+	else
+		wprintf(L"  type: compact unicode object\n");
+	wprintf(L"  length: %lu\n", PyUnicode_GET_LENGTH(p));
+	wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(p));
 }
