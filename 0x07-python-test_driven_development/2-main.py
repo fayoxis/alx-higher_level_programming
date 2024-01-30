7d21fccdad67f5a5f0f445998e5a10dc21ef8e4b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 20:03:16 +0100

    Update 2-main.py

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-div = 1
-
-matrix = ([1, 2, 3], [1, 2, 3])
-
-print(div)
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+print(matrix_divided(matrix, 3))
 print(matrix)
-print(matrix_divided(matrix, div))
