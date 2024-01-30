99084e64a1cc74a81776f9e9f247fc9d687924fe
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 03:59:25 +0100

    Update 3-rectangle.py

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,61 +1,61 @@
 #!/usr/bin/python3
-"""
-Class rectangle
+""" empty class Rectangle that defines a rectangle
 """
 
 
 class Rectangle:
-
+    """ class rectangle"""
     def __init__(self, width=0, height=0):
-
+        """ Instantiation with optional width and height"""
         self.width = width
         self.height = height
 
     @property
-    def height(self):
-
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+    def width(self):
+        """ width
+        """
+        return self.__width
 
     @property
-    def width(self):
-
-        return self.__width
+    def height(self):
+        """ height
+        """
+        return self.__height
 
     @width.setter
     def width(self, value):
-
-        if not isinstance(value, int):
+        """ width setter
+        """
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    def area(self):
+    @height.setter
+    def height(self, value):
+        """ height setter
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
 
-        return (self.__width * self.__height)
+    def area(self):
+        """ returns rectangle area"""
+        return self.__width * self.__height
 
     def perimeter(self):
-
-        if self.__width == 0 or self.__height == 0:
+        """ returns rectangle perimiter"""
+        if self.__width is 0 or self.__height is 0:
             return 0
-        return ((self.__width * 2) + (self.__height * 2))
+        return self.__width * 2 + self.__height * 2
 
     def __str__(self):
-
-        if self.__width == 0 or self.__height == 0:
-            return ("")
-
-        rectangle = []
-        for i in range(self.__height):
-            [rectangle.append("#") for j in range(self.__width)]
-            if i != self.__height - 1:
-                rectangle.append("\n")
-        return ("".join(rectangle))
+        """ return the rectangle with the character #
+        """
+        if self.__width is 0 or self.__height is 0:
+            return ""
+        return ("\n".join(["".join(["#" for i in range(self.__width)])
+                for j in range(self.__height)]))
