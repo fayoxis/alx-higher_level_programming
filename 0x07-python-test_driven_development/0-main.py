ae8d95b879ba38f0fce5ad4bcf97114285a3774c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 22:34:15 +0100

    Update 0-main.py

--- 0x07-python-test_driven_development/0-main.py
+++ 0x07-python-test_driven_development/0-main.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer(float("-nan"), 4))
-print(add_integer(2, 3))
+print(add_integer(1, 2))
 print(add_integer(100, -2))
 print(add_integer(2))
 print(add_integer(100.3, -2))
