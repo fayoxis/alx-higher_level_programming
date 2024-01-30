e304facb86cd25640e76e5623258390c3200eb93
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 22:07:18 +0100

    Update 102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -6,23 +6,20 @@
 /**
  * print_python_string - Function that prints information
  * about Python strings
- * @p: Array of PyObject pointers representing Python objects.
+ * @point: Array of PyObject pointers representing Python objects.
  */
-void print_python_string(PyObject *p)
+void print_python_string(PyObject *point)
 {
-    wprintf(L"[.] string object info\n");
-    if (strcmp(p->ob_type->tp_name, "str"))
-    {
-        wprintf(L"  [ERROR] Invalid String Object\n");
-        return;
-    }
-    
-    Py_UNICODE* unicode = PyUnicode_AS_UNICODE(p);
-    Py_ssize_t length = PyUnicode_GET_LENGTH(p);
-    Py_ssize_t i;
-    
-    for (i = 0; i < length; i++)
-    {
-        wprintf(L"  character %lu: %lc\n", i, unicode[i]);
-    }
+	wprintf(L"[.] string object info\n");
+	while(strcmp(point->ob_type->tp_name, "str"))
+	{
+		wprintf(L"  [ERROR] Invalid String Object\n");
+		return;
+	}
+	if (PyUnicode_IS_COMPACT_ASCII(point))
+		wprintf(L"  type: compact ascii\n");
+	else
+		wprintf(L"  type: compact unicode object\n");
+	wprintf(L"  length: %lu\n", PyUnicode_GET_LENGTH(point));
+	wprintf(L"  value: %ls\n", PyUnicode_AS_UNICODE(point));
 }
