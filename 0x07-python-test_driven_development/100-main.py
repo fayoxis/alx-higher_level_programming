dfbea13606d8c1c5c8b0486fbf60ab151a7873e3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 20:37:01 +0100

    Update 100-main.py

--- 0x07-python-test_driven_development/100-main.py
+++ 0x07-python-test_driven_development/100-main.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 matrix_mul = __import__('100-matrix_mul').matrix_mul
 
-m_a = [[1, 2], [3, 4]]
-m_b = [[5, 6], [7, 8]]
-
-print(matrix_mul(m_a, m_b))
+print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
 print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+
+guillaume@ubuntu:~/0x07$ ./100-main.py 
+[[7, 10], [15, 22]]
+[[13, 16]]
