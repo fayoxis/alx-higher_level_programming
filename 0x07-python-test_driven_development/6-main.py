b6d9aa7b335f89cebd23fbcff055a4c54ff74230
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 20:35:17 +0100

    Update 6-main.py

--- 0x07-python-test_driven_development/6-main.py
+++ 0x07-python-test_driven_development/6-main.py
@@ -1,18 +1,5 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a list
-"""
+max_integer = __import__('6-max_integer').max_integer
 
-
-def max_integer(list=[]):
-    """Function to find and return the max integer in a list of integers
-        If the list is empty, the function returns None
-    """
-    if len(list) == 0:
-        return None
-    result = list[0]
-    i = 1
-    while i < len(list):
-        if list[i] > result:
-            result = list[i]
-        i += 1
-    return result
+print(max_integer([1, 2, 3, 4]))
+print(max_integer([1, 3, 4, 2]))
