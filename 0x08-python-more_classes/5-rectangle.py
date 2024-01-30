098756bedc8baf53a18d94bbed3114561785be99
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 09:51:38 +0100

    Update 5-rectangle.py

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,87 +1,136 @@
 #!/usr/bin/python3
-'''
-    This script defines a Square class.
-'''
+"""
+This module defines the Rectangle class.
+"""
 
-class Square:
-    '''
-    A Square class that represents a square shape.
-    '''
+class Rectangle:
+    """
+    Represents a rectangle with a given width and height.
+    """
 
-    def __init__(self, side=0):
-        '''
-        Initializes a new instance of the Square class.
+    def __init__(self, width=0, height=0):
+        """
+        Initializes a new instance of the Rectangle class.
 
         Args:
-            side (int): The side length of the square.
-        '''
-        self.side = side
+            width (int, optional): The width of the rectangle. Defaults set to 0.
+            height (int, optional): The height of the rectangle. Defaults set to 0.
+        """
+        self.__width = width
+        self.__height = height
 
-    @property
-    def side(self):
-        '''
-        Retrieves the side length of the square.
+    def __str__(self):
+        """
+        Returns the string representating rectangle using '#' characters.
 
         Returns:
-            The side length of the square.
-        '''
-        return self.__side
+            str: The rectangle is a string.
+        """
+        rectangle = []
 
-    @side.setter
-    def side(self, value):
-        '''
-        Sets the side length of the square.
+        if self.__width == 0 or self.__height == 0:
+            return ""
+
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rectangle.append("#")
+            rectangle.append("\n")
+
+        # Remove the last newline character
+        rectangle.pop()
+
+        return "".join(rectangle)
+
+    def __repr__(self):
+        """
+        Returns the string representating rectangle.
+
+        Returns:
+            str: The rectangle representation.
+        """
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """
+        Deletes an instance of the Rectangle class.
+        """
+        print("Rectangle instance has been deleted.")
+
+    @property
+    def width(self):
+        """
+        Retrieves the width of the rectangle.
+
+        Returns:
+            int: The width of the rectangle.
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Sets the width of the rectangle.
 
         Args:
-            value (int): The side length of the square.
+            value (int): The width of the rectangle.
 
         Raises:
-            TypeError: If value is not an integer.
-            ValueError: If value is less than 0.
-        '''
+            TypeError: If width is not an integer.
+            ValueError: If width is less than 0.
+        """
         if not isinstance(value, int):
-            raise TypeError("side length must be an integer")
-        if value < 0:
-            raise ValueError("side length must be >= 0")
-        self.__side = value
+            raise TypeError("Width must be an integer.")
+        elif value < 0:
+            raise ValueError("Width must be >= 0.")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        Retrieves height of the rectangle.
+
+        Returns:
+            int: The height of the rectangle.
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Sets the height of the rectangle.
+
+        Args:
+            value (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If height is not an integer.
+            ValueError: If height is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("Height must be an integer.")
+        elif value < 0:
+            raise ValueError("Height must be >= 0.")
+        else:
+            self.__height = value
 
     def area(self):
         """
-        Computes the area of the square.
+        Calculates  area of the rectangle.
 
         Returns:
-            The area of the square.
+            int: The area of the rectangle.
         """
-        return self.__side * self.__side
+        return self.__height * self.__width
 
     def perimeter(self):
         """
-        Computes the perimeter of the square.
+        Calculates perimeter of the rectangle.
 
         Returns:
-            The perimeter of the square.
+            int: The perimeter of the rectangle.
         """
-        return self.__side * 4
-
-    def __str__(self):
-        '''
-        Returns a string representation of the Square object.
-
-        Returns:
-            A string representation of the Square object.
-        '''
-        square_string = ""
-        if self.__side == 0:
-            return square_string
+        if self.__height == 0 or self.width == 0:
+            return 0
         else:
-            for i in range(0, self.__side):
-                for j in range(0, self.__side):
-                    square_string += "*"
-                square_string += "\n"
-            return square_string[:-1]
-
-    def __repr__(self):
-        return "Square({})".format(self.__side)
-
-    def __del__(self):
-        print("Bye square...", flush=True)
+            return 2 * (self.__height + self.__width)
