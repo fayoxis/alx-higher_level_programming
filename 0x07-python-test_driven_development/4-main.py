2d23cbd9e46f50ca88cc97816178a91d61332e47
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 20:23:00 +0100

    Update 4-main.py

--- 0x07-python-test_driven_development/4-main.py
+++ 0x07-python-test_driven_development/4-main.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 print_square = __import__('4-print_square').print_square
 
-print_square((True))
-print("first")
+print_square(4)
+print("")
 print_square(10)
-print("second")
+print("")
 print_square(0)
-print("third")
+print("")
 print_square(1)
-print("fourth")
+print("")
 try:
     print_square(-1)
 except Exception as e:
     print(e)
 print("")
-
-
-
