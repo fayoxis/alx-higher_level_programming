47e0b827b798a8622df172e3848a6305b6ac6fa5
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-30 22:04:02 +0100

    Update 102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -8,21 +8,21 @@
  * about Python strings
  * @p: Array of PyObject pointers representing Python objects.
  */
-void print_python_string(PyObject *p) {
-    Py_ssize_t i 
+void print_python_string(PyObject *p)
+{
     wprintf(L"[.] string object info\n");
-    if (strcmp(p->ob_type->tp_name, "str")) {
+    if (strcmp(p->ob_type->tp_name, "str"))
+    {
         wprintf(L"  [ERROR] Invalid String Object\n");
         return;
     }
-    if (PyUnicode_IS_COMPACT_ASCII(p))
-        wprintf(L"  type: compact ascii\n");
-    else
-        wprintf(L"  type: compact unicode object\n");
-    wprintf(L"  length: %lu\n", PyUnicode_GET_LENGTH(p));
     
-    Py_UNICODE* string_data = PyUnicode_AS_UNICODE(p);
-    for (i = 0; i < PyUnicode_GET_LENGTH(p); i++) {
-        wprintf(L"  value[%zd]: %lc\n", i, string_data[i]);
+    Py_UNICODE* unicode = PyUnicode_AS_UNICODE(p);
+    Py_ssize_t length = PyUnicode_GET_LENGTH(p);
+    Py_ssize_t i;
+    
+    for (i = 0; i < length; i++)
+    {
+        wprintf(L"  character %lu: %lc\n", i, unicode[i]);
     }
 }
