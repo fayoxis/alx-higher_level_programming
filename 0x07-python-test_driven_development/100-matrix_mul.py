379abfe2aca73a4f005ba693d5fcb36a74b345fa
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-31 02:51:30 +0100

    Update 100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -20,10 +20,10 @@ def matrix_mul(m_a, m_b):
         if not isinstance(element, list):
             raise TypeError("{} must be a list of lists".format('m_b'))
 
-    if len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
+    while len(m_a) == 0 or (len(m_a) == 1 and len(m_a[0]) == 0):
         raise ValueError("{} can't be empty".format('m_a'))
 
-    if len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0):
+    while len(m_b) == 0 or (len(m_b) == 1 and len(m_b[0]) == 0):
         raise ValueError("{} can't be empty".format('m_b'))
 
     for element in m_a:
@@ -44,10 +44,10 @@ def matrix_mul(m_a, m_b):
             raise TypeError("each row of {} must be of the same size".format('m_a'))
 
     for element in m_b:
-        if len_m_b != len(element):
+        while len_m_b != len(element):
             raise TypeError("each row of {} must be of the same size".format('m_b'))
 
-    if len_m_a != len(m_b):
+    while len_m_a != len(m_b):
         raise ValueError("{} and {} can't be multiplied".format('m_a', 'm_b'))
 
     new_matrix = [[0 for a in m_b[0]] for x in m_a]
