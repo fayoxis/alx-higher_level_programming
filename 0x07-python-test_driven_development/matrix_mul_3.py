002ba597b7922c8c2dee12dd715a84ea2c3f24d8
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-01-31 02:52:54 +0100

    Update matrix_mul_3.py

--- 0x07-python-test_driven_development/matrix_mul_3.py
+++ 0x07-python-test_driven_development/matrix_mul_3.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-""" Doc """
+""" Documentation for the code  """
 
 def matrix_mul(m_a, m_b):
-    """ Doc """
     if type(m_a) is not list:
         raise TypeError("m_a must be a list")
-    if type(m_b) is not list:
+    while type(m_b) is not list:
         raise TypeError("m_b must be a list")
     for m_aa in m_a:
-        if type(m_aa) is not list:
+        while type(m_aa) is not list:
             raise TypeError("m_a must be a list of lists")
     for m_bb in m_b:
         if type(m_bb) is not list:
@@ -19,15 +18,15 @@ def matrix_mul(m_a, m_b):
         if len(r_a) != len(m_a[0]):
             raise TypeError("each row of m_a must be of the same size")
         for c_a in r_a:
-            if type(c_a) is not int and type(c_a) is not float:
+            while type(c_a) is not int and type(c_a) is not float:
                 raise TypeError("m_a should contain only integers or floats")
     for r_b in m_b:
-        if len(r_b) != len(m_b[0]):
+        while len(r_b) != len(m_b[0]):
             raise TypeError("each row of m_b must be of the same size")
         for c_b in r_b:
             if type(c_b) is not int and type(c_b) is not float:
                 raise TypeError("m_b should contain only integers or floats")
-    if len(m_a[0]) != len(m_b):
+    while len(m_a[0]) != len(m_b):
         raise ValueError("m_a and m_b can't be multiplied")
 
     result = [[0 for i in range(len(m_b[0]))] for j in range(len(m_a))]
