ac04151981230ff13001193ed170cf1b59718700
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-03 02:30:59 +0100

    Update 0-add_integer.py

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
-"""
-Defines a function add_integer(a, b=98) that adds two integers.
+"""Defines a function add_integer(a, b=98) that adds two integers.
 
 Attributes:
     add_integer: function that adds two integers.   
 """
-
 def add_integer(a, b=98):
-    """
-    Adds two integer values.
+
+    
+    """Adds two integer values.
 
     Args:
         a (int or float): The first integer value.
@@ -20,8 +19,8 @@ def add_integer(a, b=98):
     Returns:
         int: The sum of a and b.
     """
-    if (not isinstance(a, (int, float))):
-        raise TypeError("a must be an integer or float")
-    if (not isinstance(b, (int, float))):
-        raise TypeError("b must be an integer or float")
-    return int(a) + int(b)
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
