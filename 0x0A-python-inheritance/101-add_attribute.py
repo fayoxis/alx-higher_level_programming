d72ae2b45acfa3f64b5b5a57cb9185f59a009388
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-05 22:00:06 +0100

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -3,7 +3,16 @@
 
 
 def add_attribute(object, attr_name, value):
+    """Adds a new attribute to an object if it's possible.
 
+    Args:
+        object (__main__.MyClass): name of the object.
+        attr_name ('str): name of the attribute.
+        value (str): value of the attribute.
+
+    Raises:
+        TypeError:  if the object can’t have new attribute.
+    """
     # print("object type ---> {}".format(type(object)))
     # print("attr_name type ---> {}".format(type(attr_name)))
     # print("value type ---> {}".format(type(value)))
