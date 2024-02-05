e7bc1d6a260d9145dd4f86f0687ac9ae70ffa99e
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-05 21:12:46 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,41 +1,46 @@
 #!/usr/bin/python3
-"""Defines a class MyInt that inherits from int.
-MyInt is a rebel. MyInt has == and != operators inverted
+"""
+This script defines a class called MyInt that inherits from the int class.
+MyInt is a rebellious class that flips the behavior of the == and != operators.
 """
 
 
 class MyInt(int):
-    """Defines a class MyInt.
+    """
+    The MyInt class represents an integer with inverted equality operators.
 
     Args:
-        int (int): value
+        int (int): The value of the integer.
     """
     def __init__(self, value):
-        """Creates new instances of class MyInt.
+        """
+        Initializes a new instance of the MyInt class.
 
         Args:
-            value (int): integer.
+            value (int): The integer value.
         """
         self.__value = value
 
     def __eq__(self, other):
-        """The method equal
+        """
+        Overrides the equality operator.
 
         Args:
-            other (int): integer.
+            other (int): The other integer to compare.
 
         Returns:
-            boolean: True or False.
+            bool: True if the values are not equal, False otherwise.
         """
         return self.__value != other
 
     def __ne__(self, other):
-        """The method not equal
+        """
+        Overrides the inequality operator.
 
         Args:
-            other (int): integer.
+            other (int): The other integer to compare.
 
         Returns:
-            boolean: True or False
+            bool: True if the values are equal, False otherwise.
         """
         return self.__value == other
