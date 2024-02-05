84e22ef673ee8a7e5805b18a5282417278e5b6de
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-05 19:58:01 +0100

    Update 6-base_geometry.py

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Defines a class BaseGeometry based on 5-base_geometry.py"""
+"""This script defines the BaseGeometry class based on
+the code in 5-base_geometry.py"""
 
 
 class BaseGeometry:
-    """Class BaseGeometry.
-    """
+    """A class that serves as the base for geometrical calculations."""
+    
     def area(self):
-        """Area function.
+        """Calculates the area of the geometry.
 
         Raises:
-            Exception: if area is not implemented.
+            Exception: If the area method is not implemented.
         """
-        raise Exception("area() is not implemented")
+        raise Exception("The area() method is not implemented.")
