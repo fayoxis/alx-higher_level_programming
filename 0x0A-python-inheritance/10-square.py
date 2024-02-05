fbe1eec38dff01c923fe4e66b6bd49cfb2e847c2
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-05 20:56:50 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,70 +1,12 @@
 #!/usr/bin/python3
-"""Defines a class Square based on the Rectangle class.
-
-Attributes:
-    width (int): The width of the rectangle.
-    height (int): The height of the rectangle.
-"""
-
-
-from typing import Union
-
-
-class Rectangle:
-    """Defines a class Rectangle.
-
-    Args:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-    """
-
-    def __init__(self, width: int, height: int):
-        """Creates new instances of the Rectangle class.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-        """
-        self.width = width
-        self.height = height
-
-    def area(self) -> int:
-        """Calculates the area of the rectangle.
-
-        Returns:
-            int: The area of the rectangle.
-        """
-        return self.width * self.height
-
-    def __str__(self) -> str:
-        """Returns a string representation of the rectangle.
-
-        Returns:
-            str: A string representation of the rectangle.
-        """
-        return f"Rectangle({self.width}, {self.height})"
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """Defines a class Square.
+    """Type class of a square inherit a rectangle"""
 
-    Args:
-        Rectangle (Rectangle): The base rectangle class.
-    """
+    def __init__(self, size):
 
-    def __init__(self, size: int):
-        """Creates new instances of the Square class.
-
-        Args:
-            size (int): The size of one side of the square.
-        """
+        self.integer_validator("size", size)
         super().__init__(size, size)
-        self.size = size
-
-    def area(self) -> int:
-        """Calculates the area of the square.
-
-        Returns:
-            int: The area of the square.
-        """
-        return self.size ** 2
+        self.__size = size
