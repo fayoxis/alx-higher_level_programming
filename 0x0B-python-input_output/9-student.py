503b49e751e2a76a6d09d371f8035bdefcedecac
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-06 14:29:59 +0100

    Update 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,32 +1,32 @@
 #!/usr/bin/python3
-"""Module defining the class Student"""
+"""Module Student class"""
 
 
 class Student:
     """
-    Class that defines properties of student.
+    Class that defines the properties of a student.
 
     Attributes:
-        first_name (str): first name of student.
-        last_name (int): last name of student.
-        age (int): age of student.
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
     """
     def __init__(self, first_name, last_name, age):
-        """Creates new instances of Student.
+        """Creates a new instance of the Student class.
 
         Args:
-            first_name (str): first name of student.
-            last_name (int): last name of student.
-            age (int): age of student.
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Retrieves a dictionary representation of a Student instance.
+        """Returns a dictionary representation of the Student instance.
 
         Returns:
-            dict: dictionary representation.
+            dict: A dictionary representation of the Student instance.
         """
         return self.__dict__
