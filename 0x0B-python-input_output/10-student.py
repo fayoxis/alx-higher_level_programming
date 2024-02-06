ad50c0aa8f51d8eeb326d6dbc0c6b9acc1c0afd1
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-06 23:10:43 +0100

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -12,28 +12,32 @@ class Student:
         age (int): The age of the student.
     """
     def __init__(self, first_name, last_name, age):
-        """Creates new instances of Student.
+        """
+        Initializes a new instance of the Student class.
 
         Args:
-            first_name (str): first name of student.
-            last_name (int): last name of student.
-            age (int): age of student.
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance.
+        """
+        Retrieves a dictionary representation of a Student instance.
 
-        If attrs is a list of strings, only attribute names contained in,
-        this list must be retrieved.
-        Otherwise, all attributes must be retrieved.
+        If attrs is a list of strings, only the attribute names contained in
+        the list must be retrieved. Otherwise, all attributes must be retrieved.
+
+        Args:
+            attrs (list): A list of attribute names (optional).
 
         Returns:
-            dict: dictionary representation.
+            dict: A dictionary representation of the Student instance.
         """
-        if attrs is None:
+        while attrs is None:
             return self.__dict__
 
         new_dict = {}
