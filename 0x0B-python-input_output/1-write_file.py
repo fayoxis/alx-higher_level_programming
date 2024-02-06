38caeca39c9ee1471f957fda53511a028b1dde86
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-06 13:47:34 +0100

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -17,6 +17,3 @@ def write_file(filename="", text=""):
         with open(filename, 'w', encoding='utf-8') as file:
             file.write(text)
             return len(text)
-    except IOError:
-        # Handle the exception if there is an issue with writing to the file
-        return -1
