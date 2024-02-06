df385c876ef03d0199d80c5e495b5344d017c6c2
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-06 14:12:01 +0100

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,19 +1,22 @@
 #!/usr/bin/python3
-"""Module containing the function to_json_string"""
+"""
+This module contains a function called to _jsonstring.
+"""
+
+
 import json
 
 
 def to_json_string(my_obj):
-    """Returns the JSON representation of an object(string).
+    """
+    Returns the JSON representation of a string object.
 
     Args:
-        my_obj (type): object to examine.
+        my_obj (object): The object to examine.
 
     Returns:
-        str: JSON representation of object.
+        str: The JSON representation of the object.
     """
-    # print("type json.dumps(my_obj)--> {}".format(type(json.dumps(my_obj))))
-    # print("type my_obj--> {}".format(type(my_obj)))
 
-    # serializing json
+    # Serializing to JSON
     return json.dumps(my_obj)
