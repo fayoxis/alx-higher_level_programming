31768c1144d0348ffcb90c893463726062ad217f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-06 13:59:09 +0100

    Update 2-append_write.py

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Module containing the function append_write"""
+"""Module function write_file"""
 
 
-def append_write(filename="", text=""):
-    """Appends a string to a text file (UTF8) and returns the number
-    of characters added.
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) && returns the number
+    of characters.
 
     Args:
-        filename (str, optional): name of the file. Defaults to "".
+        filename (str, optional): the name of the file. Defaults to "".
         text (str, optional): string of text to write to file. Defaults to "".
 
     Returns:
-        int: number of characters appended to file.
+        int: the number o characters to file.
     """
     with open(filename, 'a', encoding="utf-8") as f:
-        """This method returns the number of characters appended to a file."""
         return f.write(text)
