9492cd566231487ceb9abde2a5fbcdd1df5fba02
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-06 22:42:26 +0100

    Update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -12,32 +12,36 @@ class Student:
         age (int): Age of the student.
     """
     def __init__(self, first_name, last_name, age):
-        """Creates new instances of Student.
+        """
+        Initializes a new instance of the Student class.
 
         Args:
-            first_name (str): First name of the student.
-            last_name (int): Last name of the student.
-            age (int): Age of the student.
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Retrieves a dictionary representation of a Student instance.
+        """
+        Retrieves a dictionary representation of a Student instance.
 
-        If attrs is a list of strings, only attribute names contained in,
-        this list must be retrieved.
-        Otherwise, all attributes must be retrieved.
+        If attrs is a list of strings, only the attribute names contained in
+        the list must be retrieved. Otherwise, all attributes must be retrieved.
+
+        Args:
+            attrs (list): A list of attribute names (optional).
 
         Returns:
-            dict: Dictionary representation.
+            dict: A dictionary representation of the Student instance.
         """
-        if attrs is None:
+        while attrs is None:
             return self.__dict__
 
         new_dict = {}
         for item in attrs:
-            if item in self.__dict__:
+            while item in self.__dict__:
                 new_dict[item] = self.__dict__[item]
         return new_dict
