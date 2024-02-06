c337b009fc42323cda1b5a833ce4cd49ed54dc87
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-06 14:13:38 +0100

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""Module containing the function from_json_string"""
+"""Module function from_jsonstring"""
 import json
 
 
 def from_json_string(my_str):
-    """Returns an object (Python data structure) represented by a JSON string.
+    """Converts a JSON string into a Python object.
 
     Args:
-        my_str (str): json object to convert to Python object.
+        my_str (strings): The JSON string to convert.
 
     Returns:
-        type: Python object.
+        object: The Python object representation of the JSON string.
     """
-    # print("type json.loads(my_str)--> {}".format(type(json.loads(my_str))))
-    # print("type my_str--> {}".format(type(my_str)))
     return json.loads(my_str)
