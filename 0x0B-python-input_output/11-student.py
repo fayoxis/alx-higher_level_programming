5372c719b50d57c9a49aa5fa85086dacedf76f3f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-06 23:26:40 +0100

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python3
-"""Module  10-teacher.py"""
+"""
+This module defines the Student class based on the 10-student.py module.
+"""
 
-
-class Teacher:
+class Student:
     """
-    Class that defines properties of a teacher.
+    A class that represents a student.
 
     Attributes:
-        first_name (str): first name of the teacher.
-        last_name (str): last name of the teacher.
-        age (int): age of the teacher.
+        first_name (str): The first name of the student.
+        last_name (str): The last name of the student.
+        age (int): The age of the student.
     """
+
     def __init__(self, first_name, last_name, age):
-        """Creates new instances of Teacher.
+        """
+        Initializes a new instance of the Student class.
 
         Args:
-            first_name (str): first name of the teacher.
-            last_name (str): last name of the teacher.
-            age (int): age of the teacher.
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
         """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Returns a dictionary representation of the Student instance.
+        """
+        Retrieves a dictionary representation of the Student instance.
 
-        If attrs is provided as a list of strings, only the attributes 
-        with names contained in the list will be included in the dictionary. 
-        If attrs is not provided, all attributes will be included.
+        If attrs is a list of strings, only attribute names contained in
+        this list will be retrieved. Otherwise, all attributes will be retrieved.
 
         Args:
-            attrs (list): A list of attribute names (optional).
+            attrs (list[str], optional): A list of attribute names to retrieve.
+                Defaults to None.
 
         Returns:
             dict: A dictionary representation of the Student instance.
@@ -43,15 +47,15 @@ class Teacher:
         for item in attrs:
             try:
                 new_dict[item] = self.__dict__[item]
-            except Exception:
+            except KeyError:
                 pass
         return new_dict
 
     def reload_from_json(self, json):
-        """Replaces all attributes of the Student instance
-        with the values from the provided JSON object.
+        """
+        Replaces all attributes of the Student instance with the given JSON.
 
         Args:
-            json (dict): A JSON object containing attribute values.
+            json (dict): A dictionary representation of the Student instance.
         """
         self.__dict__.update(json)
