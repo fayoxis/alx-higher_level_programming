aee6fa63a21fdd36a6f9ebe427550668c89b5599
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-07 00:03:43 +0100

    Update 101-stats.py

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,15 +1,5 @@
 #!/usr/bin/python3
-"""Module containing a script that reads stdin line by line and computes,
-metrics
-Each 10 lines and after a keyboard interruption (CTRL + C), prints those,
-statistics since the beginning:
-Total file size: File size: <total size>
-where is the sum of all previous (see input format above)
-Number of lines by status code:
-possible status code: 200, 301, 400, 401, 403, 404, 405 and 500
-if a status code doesn’t appear, don’t print anything for this status code
-format: <status code>: <number>
-status codes should be printed in ascending order
+"""Module containing a script that reads stdin line by line and computes
 """
 
 
