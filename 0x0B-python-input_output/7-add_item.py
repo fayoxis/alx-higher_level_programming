9d9fd5e8bdcd7a162faf6705cce73997fc55b5a4
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-02-06 14:21:00 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -15,10 +15,12 @@ def save_to_json_file(data, filename):
     with open(filename, 'w') as file:
         json.dump(data, file)
 
+
 try:
     json_list = load_from_json_file('add_item.json')
 except (ValueError, FileNotFoundError):
     json_list = []
+
 
 for item in argv[1:]:
     json_list.append(item)
