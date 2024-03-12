0edb9bf11b8ad72a7314131763e11c6df7a3901d
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-03-12 14:16:57 +0100

    Update 5-full_table.sql

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,4 +1,4 @@
 -- script 5
---  script that prints the full description of the table first_table from
--- the database hbtn_0c_0 in your MySQL server.
+-- this script  prints the full description of first_table from
+-- database hbtn_0c_0 in a MySQL server.
 SHOW CREATE TABLE first_table;
