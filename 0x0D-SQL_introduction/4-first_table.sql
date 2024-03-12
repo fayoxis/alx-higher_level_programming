0cf80f5b2ace95f6428563c86e88dd5f33f1561f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-03-12 14:16:09 +0100

    Update 4-first_table.sql\

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
 -- script 4
--- script that creates a table called first_table in the current database
+-- this script creates a table called first_table
 CREATE TABLE IF NOT EXISTS first_table(
     id INT,
     name VARCHAR(256)
