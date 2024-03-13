ffec1f6bd01aa9d9e0546743cbbdbb34cfe163de
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-03-13 14:30:55 +0100

    Update 11-genre_id_all_shows.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -1,5 +1,6 @@
--- script that lists all shows contained in the database hbtn_0d_tvshows.
-SELECT tv_shows.title, tv_show_genres.genre_id
-    FROM tv_shows LEFT JOIN tv_show_genres
-    ON tv_shows.id = tv_show_genres.show_id
-    ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+-- this script lists all shows contained on database hbtn_0d_tvshows.
+SELECT s.`title`, g.`genre_id`
+  FROM `tv_shows` AS s
+       LEFT JOIN `tv_show_genres` AS g
+       ON s.`id` = g.`show_id`
+ ORDER BY s.`title`, g.`genre_id`;
