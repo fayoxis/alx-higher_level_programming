ad15aae65491e72cb03db78e023250cb56504d83
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-03-13 14:37:21 +0100

    Update 103-rating_genres.sql

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,11 +1,10 @@
--- script that lists all shows from hbtn_0d_tvshows_rate by their rating.
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating -- Query to join tables
-FROM tv_genres
-JOIN tv_show_genres
-     ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows
-     ON tv_show_genres.show_id = tv_shows.id
-JOIN tv_show_ratings
-     ON tv_show_ratings.show_id = tv_shows.id
-GROUP BY tv_genres.name
-ORDER BY rating DESC;
+-- this script lists all shows from hbtn_0d_tvshows_rate.
+SELECT `name`, SUM(`rate`) AS `rating`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON s.`genre_id` = g.`id`
+
+       INNER JOIN `tv_show_ratings` AS r
+       ON r.`show_id` = s.`show_id`
+ GROUP BY `name`
+ ORDER BY `rating` DESC;
