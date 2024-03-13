7b42168042552611f4e56b0ff50a80932fbd12c3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-03-13 14:20:26 +0100

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- script 0
--- lists all privileges of the MySQL users user_0d_1 and user_0d_2 on
--- your server (in localhost).
+-- this is the list of privileges  MySQL users user_0d_1 and user_0d_2 on
+-- a server.
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
