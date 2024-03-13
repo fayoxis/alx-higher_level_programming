be538edc487c2b4f0c4da6be10c3530b86da560f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-03-13 14:33:51 +0100

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,10 +1,10 @@
--- script that lists all Comedy shows in the database hbtn_0d_tvshows.
-SELECT
-    tv_shows.title AS title
-    FROM tv_show_genres
-    INNER JOIN tv_shows
-    ON tv_show_genres.show_id = tv_shows.id
-    INNER JOIN tv_genres
-    ON tv_genres.id = tv_show_genres.genre_id
-    AND tv_genres.name = 'Comedy'
-    ORDER BY title;
+-- this script lists all Comedy shows in the database hbtn_0d_tvshows.
+SELECT t.`title`
+  FROM `tv_shows` AS t
+       INNER JOIN `tv_show_genres` AS s
+       ON t.`id` = s.`show_id`
+
+       INNER JOIN `tv_genres` AS g
+       ON g.`id` = s.`genre_id`
+       WHERE g.`name` = "Comedy"
+ ORDER BY t.`title`;
