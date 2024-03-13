c2c7e0545e662b424e1ce62a52193773a0df6903
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-03-13 14:36:14 +0100

    Update 102-rating_shows.sql

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,5 +1,4 @@
--- script that lists all shows without the genre Comedy in the database
--- hbtn_0d_tvshows.
+-- this script lists all shows without the genre Comedy.
 SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating -- Query to join tables
 FROM tv_shows
 JOIN tv_show_ratings
