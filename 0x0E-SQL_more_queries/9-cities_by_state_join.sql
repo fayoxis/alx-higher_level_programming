11495bcc4f3e10b77a6a24d0a496aa3478337483
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-03-13 14:29:18 +0100

    Update 9-cities_by_state_join.sql

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
--- script that lists all cities contained in database hbtn_0d_usa.
+-- this script lists all cities contained on database hbtn_0d_usa.
 SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN states ON cities.state_id=states.id
