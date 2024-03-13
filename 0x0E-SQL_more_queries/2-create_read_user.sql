c55325c8ac28ce9256ec5fb383d586e311ad0e7f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-03-13 14:24:10 +0100

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,7 @@
 -- script 2
--- creates the database hbtn_0d_2 and the user user_0d_2.
+-- this will create the database hbtn_0d_2 and it user user_0d_2.
+
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
+FLUSH PRIVILEGES;
