f8356b7abbd1f2f4c3a164ae8c241409b429ba9c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-03-13 11:12:27 +0100

    Update 8-count_89.sql

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,4 +1,4 @@
 -- script 8
--- script displays number of records  id = 89 in the
--- first_table of the database hbtn_0c_0 in a MySQL server.
+-- this script displays number of records  id = 89 in the
+-- first_table of the database hbtn_0c_0 in MySQL server.
 SELECT COUNT(*) FROM first_table WHERE id = 89;
