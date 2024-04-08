3ba1e18d556808d0daad9dcf15296ce723802e80
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-08 15:48:38 +0100

    Update 2-arguments.js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,8 @@
 #!/usr/bin/node
+
 const process = require('process');
-let message;
-if (process.argv.length === 3) {
-  message = 'Argument found';
-} else if (process.argv.length < 3) {
-  message = 'No argument';
-} else {
-  message = 'Arguments found';
-}
+
+const argumentCount = process.argv.length - 2;
+const message = argumentCount === 1 ? 'Argument found' : argumentCount < 1 ? 'No argument' : 'Arguments found';
+
 console.log(message);
