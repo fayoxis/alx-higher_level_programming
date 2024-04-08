9d6f1b252012b451995a7374ff9e6e6f0ba81466
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-08 15:52:34 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
 const process = require('process');
 const message = 'No argument';
-if (process.argv[2] === undefined) {
-  console.log(message);
-} else {
-  console.log(process.argv[2]);
-}
+
+console.log(process.argv[2] === undefined ? message : process.argv[2]);
