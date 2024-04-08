fe81efcf008b6624c3622da9665d8f3656c7b340
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-08 16:54:29 +0100

    Update 10-factorial.js

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-function factorial(n) {
+const factorial = (n) => {
   if (n === 0 || isNaN(n)) {
     return 1;
   } else {
     return n * factorial(n - 1);
   }
-}
+};
 
 const inputNumber = Number(process.argv[2]);
 console.log(factorial(inputNumber));
