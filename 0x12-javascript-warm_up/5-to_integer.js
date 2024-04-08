f1336428bd5d8eddd515336805a14a52d95a477e
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-08 15:54:32 +0100

    Update 5-to_integer.js

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,12 +1,7 @@
 #!/usr/bin/node
 const process = require('process');
-let number;
-let message = 'Not a number';
-if (process.argv.length > 2) {
-  number = parseInt(process.argv[2]);
-  if (!isNaN(number)) {
-    number = String(number);
-    message = `My number: ${number}`;
-  }
-}
+const argument = process.argv[2];
+const number = parseInt(argument);
+const message = !isNaN(number) ? `My number: ${number}` : 'Not a number';
+
 console.log(message);
