c82ed78a1d1384aa66fbdaaafc56925001ce9bf7
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-08 15:34:24 +0100

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
+const myVar = `C is fun
+Python is cool
+JavaScript is amazing`;
+
+console.log(myVar);
