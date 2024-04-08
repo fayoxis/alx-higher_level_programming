32a2123ea85b7eb83b012ea26c9657fcb8825556
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-08 16:00:41 +0100

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,14 @@
 #!/usr/bin/node
-const process = require('process');
-let numOfTimes = parseInt(process.argv[2]);
+const x = Math.floor(Number(process.argv[2]));
 const message1 = 'Missing number of occurrences';
 const message2 = 'C is fun';
 
-if (isNaN(numOfTimes)) {
+if (isNaN(x)) {
   console.log(message1);
 } else {
-  for (let i = 0; i < numOfTimes; i++) {
+  let i = x;
+  while (i > 0) {
     console.log(message2);
+    i--;
   }
 }
