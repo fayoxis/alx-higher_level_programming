65e563eb1c099a89516b497cfd45ab33ac5ccbcf
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-08 16:09:03 +0100

    Update 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,10 +2,10 @@
 const s = Math.floor(Number(process.argv[2]));
 
 if (isNaN(s)) {
-  console.log('Missing size');
+console.log('Missing size');
 } else {
-  for (let r = 0; r < s; r++) {
-    let row = 'X'.repeat(s);
-    console.log(row);
-  }
+for (let r = 0; r < s; r++) {
+let row = 'X'.repeat(s);
+console.log(row);
+}
 }
