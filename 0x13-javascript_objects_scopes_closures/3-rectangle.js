a4f6fcdc2784f5125551d5e9064be11b19bd8b81
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 12:58:04 +0100

    Update 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,23 +1,14 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
+  constructor(width, height) {
+    if (width > 0 && height > 0) {
+      [this.width, this.height] = [width, height];
     }
   }
 
-  /**
-   * @property {method} print - prints the rectangle using the character X
-   * @returns void
-   */
-  print () {
+  print() {
     for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += 'X';
-      }
-      console.log(s);
+      console.log('X'.repeat(this.width));
     }
   }
 }
