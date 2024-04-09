788bcd21f01a730f66a82a57e302f7c161fe7792
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 15:13:04 +0100

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,10 +1,4 @@
 #!/usr/bin/node
-exports.nbOccurrences = function(list, searchElement) {
-  return list.reduce((count, current) => {
-    if (current === searchElement) {
-      return count + 1;
-    } else {
-      return count;
-    }
-  }, 0);
+exports.nbOccurences = function (list, searchElement) {
+  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
 };
