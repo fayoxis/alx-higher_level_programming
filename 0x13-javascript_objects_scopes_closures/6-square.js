7607cdd0b9f07eb69b94041ded8863244828982f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 14:49:15 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
-// Import the Square module
-const Square = require('./5-square');
+const Square = require('./6-square');
 
-// Create a new instance of Square with size 4
 const s1 = new Square(4);
-
-// Call the charPrint method without passing any argument
 s1.charPrint();
 
-// Call the charPrint method and pass 'C' as the character argument
 s1.charPrint('C');
-
-// The output will remain the same as before.
