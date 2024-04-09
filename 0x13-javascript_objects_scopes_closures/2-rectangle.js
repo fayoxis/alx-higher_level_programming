5126355c126ac5d105c559aaf42e0b5033829333
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 12:32:39 +0100

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,12 +1,10 @@
-#!/usr/bin/node
 class Rectangle {
   constructor(w, h) {
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      // Create an empty object if w or h is equal to 0 or not a positive integer
-      Object.create(null);
+    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
+      return {};
     }
+
+    this.width = w;
+    this.height = h;
   }
 }
