55acccee17c0a60fa377a19bc6a670fb420905ca
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 12:15:53 +0100

    Update 1-rectangle.js

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-Class Rectangle(w, h) {
-  this.width = w;
-  this.height = h;
+class Rectangle {
+  constructor(w, h) {
+    this.width = w;
+    this.height = h;
+  }
 }
-
-Rectangle.prototype = {
-  constructor: Rectangle
-};
 
 module.exports = Rectangle;
