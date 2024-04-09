89379fa1438cc6c8db197a31f81db9b0e1f1c0b7
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 15:02:39 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
-charPrint(c) {
-  if (typeof c === 'undefined') {
-    this.print();
-  } else {
-    for (let i = 0; i < this.height; i++) {
-      console.log(c.toString().repeat(this.width));
+class Square extends require('./5-square.js') {
+  charPrint(c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 }
