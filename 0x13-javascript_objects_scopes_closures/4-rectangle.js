5c02acd72ff7f74833b947cf256a739d6ff45b34
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 13:04:53 +0100

    Update 4-rectangle.js

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,18 +1,28 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+#!/usr/bin/node
+
+class Rectangle {
+  constructor(w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
-  print () {
-    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
+  print() {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
   }
 
-  rotate () {
+  rotate() {
     [this.width, this.height] = [this.height, this.width];
   }
 
-  double () {
-    [this.width, this.height] = [this.width * 2, this.height * 2];
+  double() {
+    this.width *= 2;
+    this.height *= 2;
   }
-};
+}
+
+module.exports = Rectangle;
