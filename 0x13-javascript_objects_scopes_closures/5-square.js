84b871076f4a1c33b11de7e4a24f347862c19f56
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 15:00:00 +0100

    Update 5-square.js

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,8 +1,12 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
+class Square extends require('./5-square.js') {
+  charPrint(c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
   }
 }
-module.exports = Square;
