58d9c4f2dd8981cd959d58fb3919df5f51311555
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 16:24:14 +0100

    Update 10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-/**
- * function that converts a number from base 10 to another base passed
- * as argument
- * @param {number} base - base to examine
- * @returns {any} - the number converted to given base
- */
-exports.converter = function (base) {
-  function converted (number) {
-    return number.toString(base);
+module.exports = {
+  converter: function(base) {
+    return function(num) {
+      return num.toString(base);
+    };
   }
-  return converted;
 };
