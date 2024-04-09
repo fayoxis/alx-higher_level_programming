7596a10d76c052e31e82e7d533691b2806e0d642
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 14:58:37 +0100

    Update 6-square.js

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,22 +1,10 @@
 #!/usr/bin/node
-const ParentSquare = require('./5-square.js');
-
-class Square extends ParentSquare {
-  constructor() {
-    super();
-  }
-
-  charPrint(c) {
-    if (typeof c === 'undefined') {
-      this.print();
-    } else {
-      let square = '';
-      for (let i = 0; i < this.width; i++) {
-        square += c.repeat(this.width) + '\n';
-      }
-      console.log(square);
+charPrint(c) {
+  if (typeof c === 'undefined') {
+    this.print();
+  } else {
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.toString().repeat(this.width));
     }
   }
 }
-
-module.exports = Square;
