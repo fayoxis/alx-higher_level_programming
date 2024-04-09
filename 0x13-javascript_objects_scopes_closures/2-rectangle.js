986fdc3aeb5464ae82083c3052f66079191f14de
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 12:28:08 +0100

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,6 +1,13 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+
+'use strict';
+
+class Rectangle {
   constructor(w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
   }
-};
+}
+
+module.exports = Rectangle;
