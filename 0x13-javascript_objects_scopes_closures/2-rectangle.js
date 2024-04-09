1751277a996027adaea5d24b268b10fa992c2ea4
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 16:50:25 +0100

    Update 2-rectangle.js

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,24 +1,16 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
-  #width;
-  #height;
-
+class Rectangle {
   constructor(width, height) {
-    this.setDimensions(width, height);
+    this.width = width;
+    this.height = height;
   }
 
-  setDimensions(width, height) {
+  static create(width, height) {
     if (width > 0 && height > 0) {
-      this.#width = width;
-      this.#height = height;
+      return new Rectangle(width, height);
     }
+    return null;
   }
+}
 
-  get width() {
-    return this.#width;
-  }
-
-  get height() {
-    return this.#height;
-  }
-};
+module.exports = Rectangle;
