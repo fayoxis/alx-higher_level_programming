833a0909b8136fca3bff55b40b32c4d24804e5a7
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 15:08:17 +0100

    Update 7-occurrences.js

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,22 +1,10 @@
 #!/usr/bin/node
-/**
- * function to count number of occurrences in a list
- * @param {list} list - list ti examine
- * @param {number} searchElement - element to search for
- * @returns {number} - the number of occurrences in a list
- */
-exports.nbOccurences = function (list, searchElement) {
+exports.nbOccurrences = function(list, searchElement) {
   let count = 0;
-  list.forEach((item) => {
+  for (let item of list) {
     if (item === searchElement) {
       count++;
     }
-  });
+  }
   return count;
 };
-// alternative to arrow function
-// list.forEach(function (item) {
-//   if (item === searchElement) {
-//     i++;
-//   }
-// });
