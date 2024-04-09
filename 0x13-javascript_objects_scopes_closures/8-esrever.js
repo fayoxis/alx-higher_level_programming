dacfb18f1c2b1779714586331c48ddebed270ca3
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-09 15:41:22 +0100

    Update 8-esrever.js

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,13 +1,7 @@
 #!/usr/bin/node
-/**
- * function to reverse a list
- * @param {list} list - list to examine
- * @returns {number} - the reversed version of a list
- */
 exports.esrever = function (list) {
-  const newList = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    newList.push(list[i]);
-  }
-  return newList;
+  return list.reduceRight(function (array, current) {
+    array.push(current);
+    return array;
+  }, []);
 };
