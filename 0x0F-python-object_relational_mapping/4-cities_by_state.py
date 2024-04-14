17ed5dd2f2a6a7e8de141332f1539659aabd0cb1
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 21:11:32 +0100

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -9,13 +9,10 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
     JOIN states ON cities.state_id = states.id ORDER BY cities.id")
-    
-    # Fetch all the rows
     rows = cur.fetchall()
-    
-    # Iterate over the rows and print each one in a formatted way
+
     for city_id, city_name, state_name in rows:
         print(f"City ID: {city_id}, City Name: {city_name}, State Name: {state_name}")
-    
+
     cur.close()
     db.close()
