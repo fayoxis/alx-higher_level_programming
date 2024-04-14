2428b957da5f99dd187c4d8a364cfcc8795f0ad2
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 20:53:44 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Script that takes in an argument and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument"""
+"""Script takes  argument and displays all values in
+table where name matches the argument"""
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+while __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        while row[1] == argv[4]:
+        if row[1] == argv[4]:
             print(row)
     cur.close()
     db.close()
