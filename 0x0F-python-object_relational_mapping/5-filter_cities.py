2aa896240b0424707e932c14a520f286532848cc
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 21:39:15 +0100

    Update 5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """Script  takes the name of state in an argument and lists
 all cities """
-import mysql.connector
+import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
     # Connect to the MySQL database
-    db = mysql.connector.connect(
+    db = MySQLdb.connect(
         host="localhost",
         port=3306,
         user=argv[1],
-        password=argv[2],
-        database=argv[3],
+        passwd=argv[2],
+        db=argv[3],
         charset="utf8"
     )
 
     # Create a cursor object
-    cursor = db.cursor()
+    cur = db.cursor()
 
     # Execute the SQL query
-    cursor.execute(
+    cur.execute(
         "SELECT cities.name "
         "FROM cities "
         "JOIN states ON cities.state_id = states.id "
@@ -29,9 +29,9 @@ if __name__ == "__main__":
     )
 
     # Fetch the results and print them
-    rows = cursor.fetchall()
+    rows = cur.fetchall()
     print(", ".join(city[0] for city in rows))
 
     # Close the cursor and the database connection
-    cursor.close()
+    cur.close()
     db.close()
