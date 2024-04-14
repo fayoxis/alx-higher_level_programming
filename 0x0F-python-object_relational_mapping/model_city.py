bfbdfe586bcff3f0e8ae4e46a28955449962196b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 22:35:36 +0100

    Update model_city.py

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """this will defines the city  class """
 
-
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-Base = declarative_base()
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.schema import ForeignKey
+from model_state import Base
 
 
 class City(Base):
-    """this is a Module make for  a city"""
+    """this defines ORM class for table
+    """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'))
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
