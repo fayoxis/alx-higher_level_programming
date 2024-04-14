4c4bd33b05b05e088488b1442d4e6b25838653ab
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 20:57:02 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,15 +4,18 @@ table where name matches the argument"""
 import MySQLdb
 from sys import argv
 
-while __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
+import MySQLdb
+from sys import argv
+
+while __name__ == '__main__':
+    db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
+                         passwd=argv[2], db=argv[3])
+
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
-    id ASC".format(argv[4]))
+    cur.execute("SELECT * FROM states \
+                 WHERE name LIKE BINARY '{}' \
+                 ORDER BY states.id ASC".format(argv[4]))
     rows = cur.fetchall()
+
     for row in rows:
-        if row[1] == argv[4]:
-            print(row)
-    cur.close()
-    db.close()
+        print(row)
