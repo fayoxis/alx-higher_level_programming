6d4060a51e81809ba8cfe371050af5b12a363e8d
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 21:01:04 +0100

    Update 3-my_safe_filter_states.py

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Script that takes in arguments and displays all values in the states
-table of hbtn_0e_0_usa where name matches the argument, safe from MySQL
-injections"""
+"""Script takes arguments and displays all values in
+table  where name matches the argument, safe from MySQL"""
 import MySQLdb
 from sys import argv
 
