0c8e73e733c1dd32b086248013f179bfb5221b0b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 20:38:23 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
                            passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-    query_rows = cur.fetchall()
+    query_rows = list(cur)
     for row in query_rows:
         print(row)
     cur.close()
