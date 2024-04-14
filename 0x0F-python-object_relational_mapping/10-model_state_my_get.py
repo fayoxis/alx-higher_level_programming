7ceaeb999284272a06c3077a578caa6174a6d5a6
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 22:02:56 +0100

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 """Script prints the object with the name passed as argument from
 the database """
+
+
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
     # Check if the required command-line arguments are provided
     if len(sys.argv) < 5:
-        print("Usage: python script.py <username> <password> <database_name>
-           <state_name>")
+        print("Usage: python script.py <username> <password> <database_name> <state_name>")
         sys.exit(1)
 
     # Extract the command-line arguments
