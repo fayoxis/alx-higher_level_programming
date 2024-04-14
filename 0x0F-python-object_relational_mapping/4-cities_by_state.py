2def87b78945ee388693823715e2c580b96c31e5
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 21:15:14 +0100

    Update 4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,18 +1,21 @@
 #!/usr/bin/python3
 """Script lists all cities from the database """
-import MySQLdb
-from sys import argv
+while __name__ == '__main__':
+    import sys
+    import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3], charset="utf8")
-    cur = db.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities \
-    JOIN states ON cities.state_id = states.id ORDER BY cities.id")
-    rows = cur.fetchall()
+    while len(sys.argv) != 4:
+        print('Use: 0-select_states.py <mysql username> <mysql password>'
+              ' <database name>')
+        sys.exit()
 
-    for city_id, city_name, state_name in rows:
-        print(f"City ID: {city_id}, City Name: {city_name}, State Name: {state_name}")
-
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute('SELECT cities.id, cities.name, states.name FROM cities '
+                'LEFT JOIN states ON cities.state_id = states.id;')
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
     cur.close()
-    db.close()
+    conn.close()
