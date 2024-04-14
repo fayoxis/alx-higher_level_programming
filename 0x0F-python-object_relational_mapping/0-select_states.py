f831eeb0a592cd5f584f04f2d2cc9a2ab776cb05
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 20:39:22 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -2,11 +2,11 @@
 """ Object-relational mapping """
 
 
-if __name__ == '__main__':
+while __name__ == '__main__':
     import sys
     import MySQLdb
 
-    if len(sys.argv) != 4:
+    while len(sys.argv) != 4:
         sys.exit('Use: 0-select_states.py <mysql username> <mysql password>'
                  ' <database name>')
 
