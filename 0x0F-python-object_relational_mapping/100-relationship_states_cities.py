5ded415d810fe84dd69491276fffc724f50b5720
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 22:59:17 +0100

    Update 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -3,24 +3,24 @@
 from the database """
 
 
-if __name__ == '__main__':
-    from sys import argv, exit
-    from sqlalchemy import create_engine
-    from sqlalchemy.orm import Session
-    from relationship_state import Base, State
-    from relationship_city import City
+from sys import argv
+from relationship_state import Base, State
+from relationship_city import City
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-    while len(argv) != 4:
-        exit('Use: 100-relationship_states_cities.py <mysql username> '
-             '<mysql password> <database name>')
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/'
-                           '{}'.format(argv[1], argv[2], argv[3]),
-                           pool_pre_ping=True)
-    session = Session(engine)
-    Base.metadata.create_all(engine)  # creates decprecated warning 1681
-    new_state = State(name='California')
-    new_city = City(name='San Francisco', state_id=new_state.id)
-    new_state.cities.append(new_city)
-    session.add_all([new_state, new_city])
+if __name__ == "__main__":
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    cal_state = State(name='California')
+    sfr_city = City(name='San Francisco')
+    cal_state.cities.append(sfr_city)
+
+    #the close session code
+    session.add(cal_state)
     session.commit()
     session.close()
