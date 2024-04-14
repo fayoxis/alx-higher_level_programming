7cdeb88c69a812f3c4c4310f34fd02030555629a
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 20:56:14 +0100

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -3,7 +3,7 @@
 import MySQLdb
 from sys import argv
 
-while __name__ == "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
