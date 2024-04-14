8a6aebf48ae8447e0936acb0a770aff38abe7db5
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 21:50:44 +0100

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""Script that lists all State objects that contain the letter a from the
-database hbtn_0e_6_usa"""
+"""this Script lists all State objects that in the database """
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
-                           (sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    # Connect to the MySQL database
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    
+    # Create the database tables
     Base.metadata.create_all(engine)
+    
+    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state in session.query(State).filter(State.name.like('%a%'))\
-            .order_by(State.id):
+    
+    # Query the State table and print the id and name for states with 'a' in the name
+    for state in session.query(State).filter(State.name.like('%a%')).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
+    
+    # Close the session
     session.close()
