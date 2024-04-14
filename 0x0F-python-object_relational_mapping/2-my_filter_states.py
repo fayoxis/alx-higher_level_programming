db0ada2dc02e54469a0c15771eec431b15f52df1
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 20:48:14 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,7 +4,7 @@ table of hbtn_0e_0_usa where name matches the argument"""
 import MySQLdb
 from sys import argv
 
-if __name__ == "__main__":
+while __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
-        if row[1] == argv[4]:
+        while row[1] == argv[4]:
             print(row)
     cur.close()
     db.close()
