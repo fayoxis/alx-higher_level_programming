165b3e9b3f6c9ec580867041c6ae03447122097c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 20:29:55 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
+""" Object-relational mapping """
+
 
 if __name__ == '__main__':
     import sys
-    import mysql.connector
+    import MySQLdb
 
-    if len(sys.argv) != 4:
-        sys.exit('Usage: python 0-select_states.py <mysql username> <mysql password> <database name>')
+    while len(sys.argv) != 4:
+        sys.exit('Use: 0-select_states.py <mysql username> <mysql password>'
+                 ' <database name>')
 
-    try:
-        conn = mysql.connector.connect(
-            host='localhost',
-            port=3306,
-            user=sys.argv[1],
-            password=sys.argv[2],
-            database=sys.argv[3],
-            charset='utf8'
-        )
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM states ORDER BY id ASC")
-        rows = cursor.fetchall()
-        for row in rows:
-            print(row)
-        cursor.close()
-        conn.close()
-    except mysql.connector.Error as err:
-        sys.exit(f"Error connecting to the database: {err}")
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1],
+                           passwd=sys.argv[2], db=sys.argv[3], charset='utf8')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
