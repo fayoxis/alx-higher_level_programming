ed67ef6595764c4a6665585720abd32d62068f6b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 22:43:07 +0100

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Defines a class State"""
+"""this definitely defines the class State"""
+
 
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
-
 Base = declarative_base()
 
 
 class State(Base):
-    """Module of a state"""
+    """the module will actually state the  state"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
