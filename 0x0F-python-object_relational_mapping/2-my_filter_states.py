384361b62ea64a6c9e6e04d449d3109aba188601
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-14 20:51:45 +0100

    Update 2-my_filter_states.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,7 +4,7 @@ table of hbtn_0e_0_usa where name matches the argument"""
 import MySQLdb
 from sys import argv
 
-while __name__ == "__main__":
+if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
