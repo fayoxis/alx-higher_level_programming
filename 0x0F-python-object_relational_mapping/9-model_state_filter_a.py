6771197f8290c890a3835263ccfec3a4bb935c06
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-19 23:34:49 +0100

    Update 9-model_state_filter_a.py

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -12,17 +12,12 @@ if __name__ == "__main__":
     # Connect to the MySQL database
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    
     # Create the database tables
     Base.metadata.create_all(engine)
-    
-    # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
-    
     for state in session.query(State).filter(State.name.like(
          '%a%')).order_by(State.id):
         print("{}: {}".format(state.id, state.name))
-    
     # Close the session
     session.close()
