fad17ce69269a021dfb4d55974c5e546a95a2fe2
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-26 20:01:40 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
-"Module that fetches https://alx-intranet.hbtn.io/status"
+"here is the fetches https://alx-intranet.hbtn.io/status"
 import urllib.request
 
-
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode("utf-8")))
+    request = urllib.request.Request("https://intranet.alxswe.com/status")
+    with urllib.request.urlopen(request) as response:
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("utf-8")))
