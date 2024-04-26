b2d20526b46529d2158da89da8ab9ff4b874d3b2
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-26 21:22:26 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,7 +6,7 @@ import requests
 
 
 if __name__ == "__main__":
-    r = requests.get("https://alx-intranet.hbtn.io/status")
+    r = requests.get("https://intranet.alxswe.com/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
