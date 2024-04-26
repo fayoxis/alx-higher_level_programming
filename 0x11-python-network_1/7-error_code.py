182e1cf50fbeae921c650c51569ad294ea08d93f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-26 21:35:48 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,7 +3,11 @@
 the script will takes in a URL, sends a request to the URL
 then displays the body of the response (decoded in utf-8).
 """
+
+
 if __name__ == "__main__":
+
+    
     from requests import get
     from sys import argv
 
