e743270c7778f8fc313f295f270290688efbc98a
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-26 21:14:24 +0100

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 """
-script that fetches https://alx-intranet.hbtn.io/status.
+this script will fetches https://alx-intranet.hbtn.io/status.
 """
-import requests
+import sys
+import urllib.error
+import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    response = requests.get(url)
-    content = response.text
-    print('Body response:')
-    print('\t- type: {}'.format(type(content)))
-    print('\t- content: {}'.format(content))
+    url = sys.argv[1]
+
+    request = urllib.request.Request(url)
+    try:
+        with urllib.request.urlopen(request) as response:
+            decoded_response = response.read().decode("ascii")
+            print(decoded_response)
+    except urllib.error.HTTPError as e:
+        error_code = e.code
+            print("Error code: {}".format(error_code))
