c429bd1cba6e646af9f9629ec007bc9bc4755447
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-26 20:06:50 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 import urllib.request
 
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.alxswe.com/status")
+    request = urllib.request.Request("https://alx-intranet.hbtn.io/status")
     with urllib.request.urlopen(request) as response:
         body = response.read()
         print("Body response:")
