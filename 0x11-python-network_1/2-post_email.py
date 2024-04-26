689c205cc6c30b1002be52a3e0dc703992e5aa4b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-26 21:11:25 +0100

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-script that takes in a URL and an email, sends a POST request to the passed
-URL with the email as a parameter, and displays the body of the response
-(decoded in utf-8).
+the script that takes in a URL and an email, sends a POST request to the passed
+URL with the email as a parameter
 """
 import sys
 import urllib.parse
