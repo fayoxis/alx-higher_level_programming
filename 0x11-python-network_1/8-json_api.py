28e7d84c78ba394456415d4d3745e123a6336459
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-26 21:44:11 +0100

    Update 8-json_api.py

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,32 +1,24 @@
 #!/usr/bin/python3
 """
-script that takes in a letter and sends a POST request to
+this script will takes in a letter and sends a POST request to
 http://0.0.0.0:5000/search_user with the letter as a parameter.
-The letter must be sent in the variable q.
-If no argument is given, set q="".
-If the response body is properly JSON formatted and not empty, display the
-id and name like this: [<id>] <name>, Otherwise: Display Not a valid JSON
-if the JSON is invalid and Display No result if the JSON is empty
 """
-import sys
 import requests
-
+from sys import argv
 
 if __name__ == "__main__":
     url = "http://0.0.0.0:5000/search_user"
-    if len(sys.argv) > 1:
-        q = sys.argv[1]
-    else:
-        q = ""
-    payload = {"q": q}
+    query = argv[1] if len(argv) > 1 else ""
+    payload = {"q": query}
     response = requests.post(url, data=payload)
+
     try:
-        json_outp = response.json()
-        if not json_outp:
+        json_output = response.json()
+        if not json_output:
             print("No result")
         else:
-            my_id = json_outp.get("id")
-            my_name = json_outp.get("name")
-            print("[{}] {}".format(my_id, my_name))
-    except ValueError as invalid_json:
+            user_id = json_output.get("id")
+            user_name = json_output.get("name")
+            print(f"[{user_id}] {user_name}")
+    except ValueError:
         print("Not a valid JSON")
