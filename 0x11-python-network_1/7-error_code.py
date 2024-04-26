4b496a0ae184f88ff438783a58a212ae40805ee8
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-26 21:41:43 +0100

    Update 7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -4,14 +4,14 @@ the script will takes in a URL, sends a request to the URL
 then displays the body of the response (decoded in utf-8).
 """
 
-if __name__ == "__main__":
-    from requests import get
-    from sys import argv
+import requests
+from sys import argv
 
-    try:
-        response = get(argv[1])
-        response.raise_for_status()
-    except:
-        print('Error code: {}'.format(response.status_code))
+if __name__ == "__main__":
+    url = argv[1]
+    response = requests.get(url)
+
+    if response.status_code >= 400:
+        print(f"Error code: {response.status_code}")
     else:
         print(response.text)
