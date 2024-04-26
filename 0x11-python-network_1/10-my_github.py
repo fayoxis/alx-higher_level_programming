522f0afd09919822010ab08efb265bb4fc5b7931
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-04-26 21:47:33 +0100

    Update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -6,19 +6,12 @@ import sys
 import requests
 from requests.auth import HTTPBasicAuth
 
+
 if __name__ == "__main__":
     # Set the API endpoint URL
     api_url = "https://api.github.com/user"
-    
-    # Retrieve the username and password from command line arguments
     user_name = sys.argv[1]
     user_pass = sys.argv[2]
-    
-    # Make the GET request to the API with basic authentication
     response = requests.get(api_url, auth=HTTPBasicAuth(user_name, user_pass))
-    
-    # Parse the JSON response
     json_data = response.json()
-    
-    # Print the user's ID
     print(json_data.get("id"))
