29fc046dfe830e1c2e1a68a56acc76fd9ed8318b
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-01 00:59:52 +0100

    Update 101-post_json.sh

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sthis cript that sends a JSON POST request to a URL passed as the first argument.
+# this cript that sends a JSON POST request to a URL passed as the first argument.
 curl -s -H "Content-Type: application/json" -d "$(cat "$2")" "$1"
