3f6379a510dbd72ed579575582973999cb54d73f
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-01 00:58:38 +0100

    Update 100-status_code.sh

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
-# script that sends a request to a URL passed as an argument, and displays only the status code of the response.
+#this  script that sends a request to a URL passed as an argument
 curl -s -L -X HEAD -w "%{http_code}" "$1"
