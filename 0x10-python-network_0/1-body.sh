ceaeffebca18f1b24d3ea0d58b26ceda1a08ddf8
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-01 00:43:33 +0100

    Update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a GET request to the URL, and displays the body of the response
+# this script that takes in a URL, sends a GET request to the URL
 curl -Ls "$1"
