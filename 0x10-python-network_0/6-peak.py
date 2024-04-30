ca0c21486403e12b6117cc1aa291cf264c4144ca
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-01 00:58:06 +0100

    Update 6-peak.py

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -13,7 +13,7 @@ def find_peak(list_of_integers):
     start, end = 0, length - 1
     while start < end:
         mid = start + (end - start) // 2
-        if list_[mid] > list_[mid - 1] and list_[mid] > list_[mid + 1]:
+        while list_[mid] > list_[mid - 1] and list_[mid] > list_[mid + 1]:
             return list_[mid]
         if list_[mid - 1] > list_[mid + 1]:
             end = mid
