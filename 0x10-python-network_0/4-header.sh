17100e0667da9ed1760c2a4e943215f6e21f7d0c
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-01 00:53:48 +0100

    Update 4-header.sh

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+# this sends a GET request to the URL
 curl -s "$1" -H "X-School-User-Id: 98"
