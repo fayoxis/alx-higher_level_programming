8242d361fee7006b71a5524b227a32c3a330d5d5
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-21 20:55:01 +0100

    Update 100-starwars_characters.js

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,26 +1,26 @@
 #!/usr/bin/node
-
 const axios = require('axios');
 
-const id = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const filmId = process.argv[2];
+const filmUrl = `https://swapi-api.alx-tools.com/api/films/${filmId}`;
 
-async function getCharacterNames() {
+async function fetchCharacterNames() {
   try {
-    const response = await axios.get(`${url}${id}`);
-    const { characters } = response.data;
+    // Fetch the film data
+    const filmResponse = await axios.get(filmUrl);
+    const { characters } = filmResponse.data;
 
-    const characterNames = await Promise.all(
-      characters.map(async (characterUrl) => {
-        const characterResponse = await axios.get(characterUrl);
-        return characterResponse.data.name;
-      })
-    );
+    // Fetch the character data for each character URL
+    const characterNames = await Promise.all(characters.map(async (characterUrl) => {
+      const characterResponse = await axios.get(characterUrl);
+      return characterResponse.data.name;
+    }));
 
+    // Print the character names
     characterNames.forEach(name => console.log(name));
   } catch (error) {
     console.error(error);
   }
 }
 
-getCharacterNames();
+fetchCharacterNames();
