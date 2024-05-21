ad9796a686e582093617e945fef1ba546b1c4f72
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-21 19:59:15 +0100

    Update 0-readme.js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-const process = require('process');
+
+
 const fs = require('fs');
 
-// The first argument is the file path
-const file = process.argv[2];
-// The content of the file must be written in utf-8
-fs.readFile(file, 'utf8', function (err, data) {
-  // If an error occurred during the reading, print the error object
-  if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
-  }
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+  console.log(err || data);
 });
