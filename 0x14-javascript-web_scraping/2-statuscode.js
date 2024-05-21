212d791776ce4d9783c5d8114136edf46f069fde
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-21 20:05:07 +0100

    Update 2-statuscode.js

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,13 +1,6 @@
 #!/usr/bin/node
-const process = require('process');
-const request = require('request');
-
-// The first argument is the URL to request (GET)
+const http = require('http');
 const url = process.argv[2];
-
-request(url, function (error, response) {
-  if (error == null) {
-    // display the status code of a GET request.
-    console.log(`code: ${response.statusCode}`);
-  }
+http.get(url, (response) => {
+  console.log(`code: ${response.statusCode}`);
 });
