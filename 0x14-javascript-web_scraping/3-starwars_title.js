e6bb30bc5fc1e262d1b486e46c8beb1220251f52
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-21 20:13:23 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,20 @@
 #!/usr/bin/node
-const request = require('request');
+const http = require('http');
 const episodeNum = process.argv[2];
 const API_URL = 'https://swapi-api.hbtn.io/api/films/';
-request(API_URL + episodeNum, (err, response, body) => {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const responseJSON = JSON.parse(body);
-    console.log(responseJSON.title);
+http.get(API_URL + episodeNum, (res) => {
+  if (res.statusCode === 200) {
+    let data = '';
+    res.on('data', (chunk) => {
+      data += chunk;
+    });
+    res.on('end', () => {
+      const responseJSON = JSON.parse(data);
+      console.log(responseJSON.title);
+    });
   } else {
-    console.log(`Error code: ${response.statusCode}`);
+    console.log('Error code: ' + res.statusCode);
   }
+}).on('error', (e) => {
+  console.log(e);
 });
