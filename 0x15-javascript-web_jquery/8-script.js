d5f9ec07d1192489dc7612c5817b276ad4ff3ad9
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-29 22:28:07 +0100

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,10 +1,14 @@
-$(document).ready(function () {
-	$.getJSON(
-		"https://swapi-api.alx-tools.com/api/films/?format=json",
-		function (data) {
-			data.results.forEach(function (film) {
-				$("<li>").text(film.title).appendTo("ul#list_movies");
-			});
-		}
-	);
+$(document).ready(function() {
+  $.ajax({
+    url: "https://swapi-api.alx-tools.com/api/films/?format=json",
+    method: "GET",
+    success: function(data) {
+      data.results.forEach(function(film) {
+        $("<li>").text(film.title).appendTo("ul#list_movies");
+      });
+    },
+    error: function(xhr, status, error) {
+      console.error("Error fetching movie data:", error);
+    }
+  });
 });
