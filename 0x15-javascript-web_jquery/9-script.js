cad7ea772eaec5c561310cf7764224fa5aa7a963
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-29 22:10:39 +0100

    Update 9-script.js

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,7 +1,5 @@
-//  script that fetches from https://fourtonfish.com/hellosalut/?lang=fr and
-// displays the value of hello from that fetch in the HTML tag DIV#hello.
-const $ = window.$;
-$.get('https://fourtonfish.com/hellosalut/?lang=fr', function (data, status) {
-  console.log(data.hello);
-  $('#hello').html(data.hello);
+$(document).ready(function () {
+	$.getJSON("https://fourtonfish.com/hellosalut/?lang=fr", function (data) {
+		$("DIV#hello").text(data.hello);
+	});
 });
