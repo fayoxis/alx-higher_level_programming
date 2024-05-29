fadae7b157fa835e260e9aebda34f4ef34bc89be
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-29 22:06:31 +0100

    Update 5-script.js

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,6 +1,5 @@
-// script that adds a <li> element to a list when the user clicks on the
-// tag DIV#add_item
-const $ = window.$;
-$('#add_item').bind('click', function () {
-  $('UL.my_list').append('<li>Item</li>');
+$(document).ready(function () {
+	$("DIV#add_item").click(function () {
+		$("<li>").text("Item").appendTo("ul.my_list");
+	});
 });
