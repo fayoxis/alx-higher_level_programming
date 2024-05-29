de85bef26a31852376a219c9d9842774783f3472
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-29 22:09:12 +0100

    Update 7-script.js

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,6 +1,8 @@
-// script that fetches the character name from this URL:
-// https://swapi-api.hbtn.io/api/people/5/?format=json
-const $ = window.$;
-$.get('https://swapi-api.hbtn.io/api/people/5/?format=json', function (data) {
-  $('#character').append(data.name);
+$(document).ready(function () {
+	$.getJSON(
+		"https://swapi-api.alx-tools.com/api/people/5/?format=json",
+		function (data) {
+			$("DIV#character").text(data.name);
+		}
+	);
 });
