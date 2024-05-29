b85c0d645c75429c34ceb60deb40275078290dff
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-29 22:07:46 +0100

    Update 6-script.js

--- 0x15-javascript-web_jquery/6-script.js
+++ 0x15-javascript-web_jquery/6-script.js
@@ -1,6 +1,5 @@
-// script that updates the text of the <header> element to New Header!!!
-//  when the user clicks on DIV#update_header
-const $ = window.$;
-$('#update_header').bind('click', function () {
-  $('header').replaceWith('New Header!!!');
+$(document).ready(function () {
+	$("DIV#update_header").click(function () {
+		$("header").text("New Header!!!");
+	});
 });
