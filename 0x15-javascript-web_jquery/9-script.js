c03db1a8bb295ab66a42b8a15020875ba843a1a2
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-29 22:27:07 +0100

    Update 9-script.js

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,13 @@
-$(document).ready(function () {
-	$.getJSON("https://fourtonfish.com/hellosalut/?lang=fr", function (data) {
-		$("DIV#hello").text(data.hello);
-	});
+$(document).ready(function() {
+  $.ajax({
+    url: "https://fourtonfish.com/hellosalut/?lang=fr",
+    method: "GET",
+    dataType: "json",
+    success: function(data) {
+      $("DIV#hello").text(data.hello);
+    },
+    error: function(xhr, status, error) {
+      console.error("Error fetching data:", error);
+    }
+  });
 });
