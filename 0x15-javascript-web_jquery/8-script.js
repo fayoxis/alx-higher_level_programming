26fb8e8d5771e660bb11fa065d13aa191a677c66
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-29 22:09:47 +0100

    Update 8-script.js

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,9 +1,10 @@
-//  script that fetches and lists the title for all movies by using this URL:
-//  https://swapi-api.hbtn.io/api/films/?format=json
-const $ = window.$;
-$.get('https://swapi-api.hbtn.io/api/films/?format=json', function (data) {
-  console.log(data);
-  data.results.forEach(film => {
-    $('UL#list_movies').append(film.title);
-  });
+$(document).ready(function () {
+	$.getJSON(
+		"https://swapi-api.alx-tools.com/api/films/?format=json",
+		function (data) {
+			data.results.forEach(function (film) {
+				$("<li>").text(film.title).appendTo("ul#list_movies");
+			});
+		}
+	);
 });
