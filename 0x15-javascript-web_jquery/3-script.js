3e541f5db095381051f401f8da583f540ff515f8
Tchaye Arthur <81367794+fayoxis@users.noreply.github.com>
2024-05-29 22:01:27 +0100

    Update 3-script.js

--- 0x15-javascript-web_jquery/3-script.js
+++ 0x15-javascript-web_jquery/3-script.js
@@ -1,6 +1,5 @@
-// script that adds the class red to the <header> element when the user
-// clicks on the tag DIV#red_header
-const $ = window.$;
-$('#red_header').bind('click', function () {
-  $('header').addClass('red');
+$(document).ready(function () {
+	$("DIV#red_header").click(function () {
+		$("header").addClass("red");
+	});
 });
